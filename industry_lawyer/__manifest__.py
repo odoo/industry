5b5a2f8ce460f429585207d02b581a0d622e287f
Pierre Masereel <pim@odoo.com>
2023-10-24 11:38:43 +0200

    [ADD] industry_lawyer: new industry lawyer module
    This module insall a configuration that preset the modules and
    configuration of a law firm.

---
+++ industry_lawyer/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Lawyer Industry',
+    'version': '1.0',
+    'category': 'Industry',
+    'description': u"""
+    This module insall a configuration that preset the modules and
+    configuration of a law firm.
+""",
+    'author': 'Lawyer Industry',
+    'depends': [
+        'documents_project',
+        'hr_timesheet',
+        'knowledge',
+        'project',
+        'project_enterprise',
+        'sale_management',
+        'sale_planning',
+        'sale_timesheet_enterprise',
+        'website_appointment',
+        'theme_clean',
+    ],
+    'data': [
+        'data/ir_attachment_pre.xml',
+        'data/documents_folder.xml',
+        'data/project_project.xml',
+        'data/product_template.xml',
+        'data/sale_order_template.xml',
+        'data/sale_order_template_line.xml',
+        'data/knowledge_cover.xml',
+        'data/knowledge_article.xml',
+        'data/appointment_type.xml',
+    ],
+    'demo': [
+        'demo/ir_attachment_post.xml',
+        'demo/res_partner.xml',
+        'demo/hr_employee.xml',
+        'demo/sale_order.xml',
+        'demo/sale_order_line.xml',
+        'demo/res_users.xml',
+        'demo/appointment_type.xml',
+        'demo/website.xml',
+        'demo/website_page.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
