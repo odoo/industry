c30fc337f14c0ac55d0362384b24547f5b0a79fe
Harsh Modi <hamo@odoo.com>
2023-10-25 12:52:48 +0530

    [ADD] non_profit_organization: Non Profit Organisation
    This industry module pre-configure odoo for non profit organizations.
    
    Task-Id: 3549835

---
+++ non_profit_organization/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Non Profit Organization',
+    'version': '1.0',
+    'category': 'Industry',
+    'description': """
+    Non Profit Organization
+
+    This industry module pre-configure odoo for non profit organizations.
+""",
+    'author': 'Industry-Non Profit Organization',
+    'depends': [
+        'knowledge',
+        'mass_mailing',
+        'sale_subscription',
+        'website_crm',
+        'website_event_sale',
+        'theme_treehouse',
+    ],
+    'data': [
+        'data/filters.xml',
+        'data/account_analytic_plan.xml',
+        'data/mail_template.xml',
+        'data/product_template.xml',
+        'data/product_product.xml',
+        'data/product_pricelist.xml',
+        'data/product_pricelist_item.xml',
+        'data/sale_subscription_plan.xml',
+        'data/sale_subscription_pricing.xml',
+        'data/ir_actions_server.xml',
+        'data/base_automation.xml',
+        'data/knowledge_article.xml',
+        'data/knowledge_article_attachments.xml',
+        'data/res_config_settings.xml',
+    ],
+    'demo': [
+        'demo/event_event.xml',
+        'demo/website.xml',
+        'demo/website_page_views.xml',
+        'demo/website_page.xml',
+        'demo/website_menu.xml',
+        'demo/website_theme_apply.xml',
+        'demo/website_attachment.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
