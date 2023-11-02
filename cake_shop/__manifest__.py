f8fe311a1ac129c230ca3503e91d98ecbd55c8d1
kir-odoo <kir@odoo.com>
2023-11-02 19:16:37 +0530

    [ADD] cake_shop: cake shop
    This setup is for bakery shop&nbsp;companies selling to consumers.
    Bakery are businesses that carry a large selection of products: puffs ,cakes, pastries etc...
    
    TaskId: 3460427

---
+++ cake_shop/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cake Shop',
+    'version': '1.0',
+    'category': 'Industry',
+    'description': """
+This setup is for bakery shop companies selling to consumers. Bakery are businesses that carry a large selection of products: puffs , cakes , pastries etc... 
+""",
+    'author': 'Odoo S.A.',
+    'depends': [
+        'knowledge',
+        'pos_sale',
+        'purchase_stock',
+        'sale_mrp',
+        'sale_purchase',
+        'website_sale_product_configurator',
+        'website_sale_stock',
+        'theme_bistro',
+    ],
+    'data': [
+        'data/ir_attachment_pre.xml',
+        'data/product_category.xml',
+        'data/product_template.xml',
+        'data/product_product.xml',
+        'data/knowledge_cover.xml',
+        'data/knowledge_article.xml',
+        'data/mrp_bom.xml',
+        'data/mrp_bom_line.xml',
+        'data/res_config_settings.xml',
+    ],
+    'demo': [
+        'demo/ir_attachment.xml',
+        'demo/mrp_order_point.xml',
+        'demo/mrp_order_point_order.xml',
+        'demo/website.xml',
+        'demo/res_partner.xml',
+        'demo/product_supplierinfo.xml',
+        'demo/purchase_order.xml',
+        'demo/sale_order.xml',
+        'demo/website_view.xml',
+        'demo/website_theme_apply.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
