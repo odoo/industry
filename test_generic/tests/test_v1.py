2af712a1d40a4789f66a514cee76e3cffbba2cdb
vava-odoo <vava@odoo.com>
2023-12-04 17:21:39 +0100

    [ADD] test_generic: add test module for industry

---
+++ test_generic/tests/test_v1.py
@@ -0,0 +1,7 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import BaseCase
+
+class BasicTest(BaseCase):
+    def test_dummy(self):
+        self.assertTrue(True)
