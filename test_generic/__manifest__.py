89ccdce0e9e4d7a2e18a86f22a2525b540bf6987
vava-odoo <vava@odoo.com>
2023-12-04 17:21:39 +0100

    [ADD] test_generic: add test module for industry

---
+++ test_generic/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'test',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test Odoo code with various linters.""",
+    'depends': ['base'],
+    'installable': True,
+    'license': 'LGPL-3',
+}
