6b496159607786ac968f496247de0f3cd062ded5
vava-odoo <vava@odoo.com>
2023-12-06 18:14:05 +0100

    [IMP] test_generic: improve check manifest and check mandatory files

---
+++ test_generic/tests/industry_case.py
@@ -0,0 +1,23 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import os
+
+from odoo.tests.common import TransactionCase
+
+def get_modules():
+    industry_path = os.path.abspath(__file__).split('test_generic/')[0]
+    industries = [
+        m for m in os.listdir(industry_path)
+        if os.path.isdir(industry_path + m)
+        and os.path.isdir(industry_path + m + '/data')
+    ]
+    return industries
+
+
+class IndustryCase(TransactionCase):
+
+    def setUp(cls):
+        super().setUp()
+        cls.installed_modules = cls.env['ir.module.module'].search(
+            [('name', 'in', get_modules()), ('state', '=', 'installed')]
+        ).mapped('name')
--- test_generic/tests/test_manifest.py
+++ test_generic/tests/test_manifest.py
@@ -1,46 +1,68 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import os
 from odoo.addons.test_lint.tests.test_manifests import ManifestLinter
-from odoo.tests.common import tagged, TransactionCase
+from odoo.tests.common import tagged
+
+from .industry_case import IndustryCase
 
 CATEGORIES = ('Services', 'Retail', 'Manufacturing', 'eCommerce', 'Public', 'NGO')
 
-def get_modules():
-    industry_path = os.path.abspath(__file__).split('test_generic/')[0]
-    industries = [
-        m for m in os.listdir(industry_path)
-        if os.path.isdir(industry_path + m)
-        and os.path.isfile(industry_path + m + '/__manifest__.py')
-    ]
-    industries.remove('test_generic')
-    return industries
+MANDATORY_KEYS = {
+    'category': '',
+    'data': [],
+    'demo': [],
+    'description': '',
+    'depends': [],
+    'images': [],
+    'license': '',
+    'name': '',
+    'version': '',
+}
 
 
 @tagged('post_install', '-at_install')
-class BasicTest(ManifestLinter, TransactionCase):
+class ManifestTest(ManifestLinter, IndustryCase):
 
     def test_manifests(self):
-        installed_modules = self.env['ir.module.module'].search(
-            [('name', 'in', get_modules()), ('state', '=', 'installed')]
-        ).mapped('name')
-        for module in installed_modules:
+        for module in self.installed_modules:
             with self.subTest(module=module):
                 manifest_data = self._load_manifest(module)
                 self._test_manifest_keys(module, manifest_data)
                 self._test_manifest_values(module, manifest_data)
 
+    def _test_manifest_keys(self, module, manifest_data):
+        res = super()._test_manifest_keys(module, manifest_data)
+        for key in MANDATORY_KEYS:
+            self.assertIn(key, manifest_data, "Missing key %s in manifest" % key)
+
     def _test_manifest_values(self, module, manifest_data):
         res = super()._test_manifest_values(module, manifest_data)
         for key in manifest_data:
             value = manifest_data[key]
+            expected_type = type(MANDATORY_KEYS[key])
+            self.assertEqual(
+                type(value),
+                expected_type,
+                "Wrong type for manifest value %s in module %s, expected %s" % (
+                    key, module, expected_type
+                )
+            )
             if key == 'category':
                 self.assertIn(
                     value, CATEGORIES, (
-                        "Wrong category %s, it should be one of the following: %s" % (
-                            value,
-                            ", ".join(cat for cat in CATEGORIES),
+                        "Wrong category %s in manifest, it should be one of the following: %s" % (
+                            value, ", ".join(cat for cat in CATEGORIES),
                         )
                     )
+                )
+            elif key == 'images':
+                self.assertEqual(
+                    value,
+                    ['images/main.png'],
+                    "Wrong images %r in manifest, it should be ['images/main.png']" % value
+                )
+            elif key == 'license':
+                self.assertEqual(
+                    value, 'OPL-1', "Wrong license %r in manifes, it should be 'OPL-1'" % value
                 )
         return res
