8361aa0fe4bd19e05f6a25a7a35b71a4e18fb203
vava-odoo <vava@odoo.com>
2024-04-15 10:49:02 +0200

    [IMP] tests: check payment provider state and availability
    For demonstration purposes, we enforce the installation on the payment
    provider demo. By default, this provider is enabled in test mode and
    published. But in production databases, this provider should not be
    active nor published.
    
    This commit makes sure the demo payment provider is disabled and
    unpublished in data, and enabled (in test mode) and published in demo.
    
    Task-3839550

--- tests/test_generic/tests/__init__.py
+++ tests/test_generic/tests/__init__.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import industry_case
+from . import test_industry_requirements
 from . import test_mandatory_files
 from . import test_manifest
 from . import test_xml
