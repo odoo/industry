39e3f890c2e4a37d23e4bf3afb8708b788a7d4ff
Dhrumil Shah <dhrs@odoo.com>
2024-07-02 15:37:17 +0530

    [FIX] *: install payment_demo in demo instead of dependency
    The current implementation adds payment_demo in the dependencies in the
    manifest of a few modules. This is useful in demo to showcase the whole
    checkout flow, but is polluting production databases with a useless
    module.
    
    In this commit, we change this by installing payment_demo only in demo
    databases and remove its dependency.
    
    A test ensures that payment_demo is not added as a dependency anymore
    and requires the module to be installed as soon as website_sale is
    installed.
    
    Task-4018764

--- tests/test_generic/tests/test_industry_requirements.py
+++ tests/test_generic/tests/test_industry_requirements.py
@@ -12,26 +12,15 @@ from .industry_case import IndustryCase
 class TestEnv(IndustryCase):
 
     def test_payment_demo(self):
-        if self.env['ir.module.module']._get('payment_demo').state != 'installed':
-            return
-        demo_provider = self.env.ref('payment.payment_provider_demo')
         sys_args = sys.argv
         index_database = sys_args.index('-d')
-        if sys_args[index_database + 1].endswith('imported_no_demo'):
-            self.assertEqual(
-                demo_provider.state, 'disabled', "Payment provider demo should be disabled in data"
-            )
-            self.assertFalse(
-                demo_provider.is_published, "Payment provider demo should be unpublished in data"
-            )
-        elif sys_args[index_database + 1].endswith('imported_with_demo'):
-            self.assertEqual(
-                demo_provider.state,
-                'test',
-                "Payment provider demo should be enabled (test) in demo",
-            )
+        if not sys_args[index_database + 1].endswith('imported_with_demo'):
+            return
+        if self.env['ir.module.module']._get('website_sale').state == 'installed':
             self.assertTrue(
-                demo_provider.is_published, "Payment provider demo should be published in demo"
+                self.env['ir.module.module']._get('payment_demo').state == 'installed',
+                "Payment Demo module should be installed in demo when Website Sale is installed. "
+                "Call 'button_immediate_install' on 'base.module_payment_demo' in demo."
             )
 
     def test_knowledge_article_notification(self):
