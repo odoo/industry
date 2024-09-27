acbe0e5fc1dd5a384b37a19d871bc325e1ff8a1c
Dhrumil Shah <dhrs@odoo.com>
2024-09-27 12:04:53 +0530

    [ADD] odoo_partner: add a module for Odoo partners
    This industry package includes everything you need to run your Odoo
    partnership business efficiently.
    
    Basics
    - Use the CRM app to manage leads, opportunities, and your sales
    pipeline.
    - Leverage the Sales app with advanced Quote Calculator and PDF Quote
    Builder for professional proposals.
    - Manage Success Packs and large implementations with the Project app.
    - Store and organize crucial documents in the Documents app.
    - Schedule client meetings effortlessly with the Appointment app.
    - Handle client support requests using the Helpdesk app.
    - Track billable hours with the Timesheet app and manage team schedules
    with the Planning app.
    - Streamline your billing process with the Invoicing app.
    - Addition of Project Description & Project Phasing fields on SO to
    input the PDF Quotation.
    
    This industry relies only on standard Odoo features and does not
    include any customization.
    
    task-4184872

--- tests/test_generic/tests/test_industry_requirements.py
+++ tests/test_generic/tests/test_industry_requirements.py
@@ -35,7 +35,7 @@ class TestEnv(IndustryCase):
             )
             notif = self.env['mail.message'].browse(ref.id)
             self.assertIn(
-                '<a href="/knowledge/article/',
+                '/knowledge/article/',
                 notif.body,
                 "The notification should contain a link to the knowledge article.",
             )
@@ -50,7 +50,7 @@ class TestEnv(IndustryCase):
                 "The knowledge article should be in the favorite category",
             )
             self.assertIn(
-                'href="/knowledge/article/%s' % knowledge_article.id,
+                '/knowledge/article/%s' % knowledge_article.id,
                 notif.body,
                 "The notification link should target the module-related knowledge article.",
             )
