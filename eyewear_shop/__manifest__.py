b4c6d0aa0e4b7973d4e2a2b2d65a9a07e4934df4
Dhrumil Shah <dhrs@odoo.com>
2024-10-21 11:30:56 +0530

    [ADD] eyewear_shop: added js tour for sale order cash flow and a button in knowledge article

--- eyewear_shop/__manifest__.py
+++ eyewear_shop/__manifest__.py
@@ -45,6 +45,7 @@ The sales process involves creating sale orders, managing deliveries and invoici
         'data/website_theme_apply.xml',
         'data/ir_model_data.xml',
         'data/knowledge_tour.xml',
+        'data/sale_order_to_cash_tour.xml',
     ],
     'demo': [
         'demo/res_config_settings.xml',
@@ -80,12 +81,14 @@ The sales process involves creating sale orders, managing deliveries and invoici
     'assets': {
         'web.assets_backend': [
             'eyewear_shop/static/src/js/my_tour.js',
+            'eyewear_shop/static/src/js/tours/sale_order_to_cash_tour.js',
         ]
     },
     'author': 'Odoo S.A.',
     "cloc_exclude": [
         "data/knowledge_article.xml",
         "static/src/js/my_tour.js",
+        "static/src/js/tours/sale_order_to_cash_tour.js",
     ],
     'images': ['images/main.png'],
 }
