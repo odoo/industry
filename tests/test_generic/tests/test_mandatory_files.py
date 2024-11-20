77eabca9b498f867ff19d1c3cab6bf692fa56841
vava-odoo <vava@odoo.com>
2024-11-20 17:56:04 +0100

    [IMP] test_generic: check translation files

--- tests/test_generic/tests/test_mandatory_files.py
+++ tests/test_generic/tests/test_mandatory_files.py
@@ -1,7 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import os
+from pathlib import Path
+import re
 
+from odoo import release
 from odoo.tests.common import tagged
 
 from .industry_case import IndustryCase, get_industry_path
@@ -15,7 +18,22 @@ class FileTest(IndustryCase):
             required_files = {
                 'icon': '/static/description/icon.png',
                 'image': '/images/main.png',
+                'pot file': f'/i18n/{module}.pot',
             }
             for f, path in required_files.items():
                 is_file = os.path.isfile(get_industry_path() + module + path)
                 self.assertTrue(is_file, "Missing %s at %s" % (f, module + path))
+
+            tx_config = Path(get_industry_path() + '.tx/config').read_text(encoding="utf-8")
+            self.assertTrue(re.search(module, tx_config), "Missing module in .tx/config")
+            if release.version_info[3] != 'final':
+                # skip test if master
+                continue
+            odoo_version = release.serie
+            if odoo_version.split('.')[1] != '0':
+                odoo_version = odoo_version.replace('.', '-').split('~')[-1]
+            else:
+                odoo_version = odoo_version.split('.')[0]
+            self.assertTrue(
+                re.search(odoo_version + ':r:' + module, tx_config), "Wrong version in .tx/config"
+            )
