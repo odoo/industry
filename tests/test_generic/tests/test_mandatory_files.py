b8608aa879b3e81d59186b781ec965adca4431a1
vava-odoo <vava@odoo.com>
2025-02-13 16:44:14 +0000

    [IMP] tests: add check for pot update
    When updating a module in stable, the translation pot file should
    sometimes be updated as well for terms to be sent to transifex and
    translated. As it is not automated, this can be forgotten in the commit.
    
    This commit therefore adds a test that verifies that the pot contains
    all translatable terms.
    
    X-original-commit: c0c96fafa3e7a5bd706428ef4f119388f583222c

--- tests/test_generic/tests/test_mandatory_files.py
+++ tests/test_generic/tests/test_mandatory_files.py
@@ -1,11 +1,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from difflib import unified_diff
+import io
 import os
 from pathlib import Path
 import re
 
 from odoo import release
-from odoo.tests.common import tagged
+from odoo.tests import get_db_name, tagged
+from odoo.tools.translate import trans_export
 
 from .industry_case import IndustryCase, get_industry_path
 
@@ -38,3 +41,14 @@ class FileTest(IndustryCase):
             self.assertTrue(
                 re.search(odoo_version + ':r:' + module, tx_config), "Wrong version in .tx/config"
             )
+
+            db_name = get_db_name()
+            if not db_name.endswith('imported_with_demo'):
+                return
+            with io.BytesIO() as buf:
+                trans_export(False, [module], buf, 'po', self.env.cr)
+                new = buf.getvalue().decode("utf-8")
+            old = Path(get_industry_path() + module + '/i18n/' + module + '.pot').read_text(encoding="utf-8")
+            diff = list(unified_diff(old, new))
+            diff_str = ''.join(d.replace('+','').replace('-','') for d in diff)
+            assert len(diff_str.split('\n')) < 6, "You forgot to export the pot file.\n" + diff_str
