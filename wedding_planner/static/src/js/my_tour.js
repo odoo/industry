fc81a281dbbd13f383561cec59f63b89f9ca4b1d
Dhrumil Shah <dhrs@odoo.com>
2025-03-24 11:49:27 +0530

    [ADD] wedding_planner: Wedding Planner
    Odoo for Wedding Planners
    
    This industry package includes all the essential apps and configurations for
    Wedding Planners to organise their business, follow-up with customers,
    and plan weddings.
    
    Basics:
    - Use the CRM app to track your leads and prospects
    - Create beautiful quotations through the Sales App
    - Manage Weddings, and everything surrounding it through the Projects App
    - Manage contracts & agreements through the Sign App
    - Invoice your customer in seconds through the Invoices app
    
    Task-4589604

---
+++ wedding_planner/static/src/js/my_tour.js
@@ -0,0 +1,13 @@
+import { _t } from "@web/core/l10n/translation";
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("wedding_planner_knowledge_tour", {
+    url: "/odoo",
+    steps: () => [
+        {
+            trigger: '.o_app[data-menu-xmlid="knowledge.knowledge_menu_root"]',
+            content: _t("Get on track and explore our recommendations for your Odoo usage here!"),
+            run: "click",
+        },
+    ],
+});
