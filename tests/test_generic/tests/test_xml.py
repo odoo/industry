2e0d08a58059a405fe624e950f69288e1482d7d7
Dhrumil Shah <dhrs@odoo.com>
2025-04-14 12:30:14 +0000

    [IMP] *: remove company_id field when useless
    This commit ensures company_id is not defined in some models where it
    defaults to the current company. It's mostly useless, except in a
    multi-company setup. It also adds a test to check for the presence of
    the field in specific models.
    
    Task-4720995
    
    X-original-commit: d61a3d86a76c89e9c05bbacceb7ba042c7f2c4e9
    Part-of: odoo/industry#573
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>
    Signed-off-by: Dhrumil Shah (dhrs) <dhrs@odoo.com>

--- tests/test_generic/tests/test_xml.py
+++ tests/test_generic/tests/test_xml.py
@@ -18,13 +18,15 @@ MAX_FILE_SIZE = 100 * 1024 * 1024  # in megabytes
 EXCLUDED_READONLY_FIELDS = {'lot_id', 'url', 'user_id',}
 
 USELESS_FIELDS = {
+    'account.analytic.account': ['company_id'],
     'account.analytic.plan': ['color'],
     'crm.lead': [
-        'city', 'street', 'zip', 'state_id', 'country_id', 'email', 'email_from', 'contact_name',
-        'partner_name', 'title', 'function', 'website', 'street2', 'phone'
+        'city', 'street', 'zip', 'state_id', 'country_id', 'email', 'email_from', 'mobile', 'contact_name',
+        'partner_name', 'title', 'function', 'website', 'street2', 'phone', 'company_id'
     ],
     'crm.tag': ['color'],
     'hr.applicant': ['last_stage_id'],
+    'hr.employee': ['company_id'],
     'ir.attachment': ['access_token'],
     'ir.model.fields': ['model'],
     'knowledge.article': ['article_member_ids'],
@@ -39,14 +41,15 @@ USELESS_FIELDS = {
     'product.product': ['lst_price'],
     'product.template.attribute.value': ['color'],
     'project.tags': ['color'],
+    'project.task': ['company_id'],
     'purchase.order': ['currency_id', 'state'],
-    'purchase.order.line': ['date_planned', 'move_dest_ids', 'name'],
+    'purchase.order.line': ['date_planned', 'move_dest_ids', 'name', 'company_id'],
     'res.partner': ['tz'],
     'sale.order': [
         'access_token', 'date_order', 'health', 'origin', 'partner_invoice_id', 'partner_shipping_id',
-        'validity_date', 'warehouse_id',
+        'validity_date', 'warehouse_id', 'company_id'
     ],
-    'sale.order.line': ['qty_delivered'],
+    'sale.order.line': ['qty_delivered', 'company_id'],
     'sign.template': ['name'],
     'worksheet.template': ['color'],
 }
@@ -351,6 +354,8 @@ class TestEnv(IndustryCase):
                 field = model._fields.get(field_name)
                 useless = USELESS_FIELDS.get(model_name, [])
                 if field_name in useless:
+                    if field_name == 'company_id' and self.env['res.company'].search_count([]) > 1:
+                        continue  # company_id only matters in a multi-company setup
                     if 'crm.lead' in self.env and model == self.env['crm.lead']:
                         if 'partner_id' in fields_set_in_record:
                             _logger.warning(
