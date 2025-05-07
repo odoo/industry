800e14c5f78912839627155d976b7718224f3f05
vava-odoo <vava@odoo.com>
2025-05-07 18:58:33 +0200

    [IMP] test_generic: scheduled actions should be updatable
    closes odoo/industry#618
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- tests/test_generic/tests/test_xml.py
+++ tests/test_generic/tests/test_xml.py
@@ -65,6 +65,7 @@ MODELS_TO_UPDATE = [
     "ir.actions.act_window",
     "ir.actions.report",
     "ir.actions.server",
+    "ir.cron",
     "ir.model",
     "ir.model.access",
     "ir.model.fields",
