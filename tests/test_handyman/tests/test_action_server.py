4e91bddc5377e0040686ff7e4559adfa76870481
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-13 16:00:30 +0200

    [IMP] tests: test_handyman working tests (error x_section_id not computed)

--- tests/test_handyman/tests/test_action_server.py
+++ tests/test_handyman/tests/test_action_server.py
@@ -26,14 +26,48 @@ class ActionServerTestCase(TransactionCase):
                 }).id,
             }).id,
         })
+        cls.sale_order_1 = cls.env['sale.order'].create({
+            'name': 'order_item_1',
+            'partner_id': cls.env['res.partner'].create({
+                'name': 'partner_a',
+            }).id,
+            'order_line': [Command.create({'name': 'section_1', 'display_type': 'line_section'})]
+        })
+        cls.sale_order_1.order_line = [Command.link(cls.env['sale.order.line'].create({
+            'name': 'sale_line_1',
+            'product_id': cls.env.ref("handyman.product_product_5").id,
+            'order_id': cls.sale_order_1.id,
+            'x_section_id': False,
+        }).id)]
+        #print(cls.sale_order_1.order_line.mapped('name'))
+        #print("\n", cls.sale_order_1.order_line[1].x_section_id)
+        cls.sale_order_1.order_line[1].x_section_id = cls.sale_order_1.order_line[0]
+        #print(cls.env.ref("handyman.x_section_id_field").compute)
+        cls.sale_order_1.action_confirm()
+        #print(cls.sale_order_1.order_line[1].x_section_id, "\n")
+
+    def test_base_automation_create_move_line_add_automatic_account(self):
+        self.move_line = self.demo_account_move.invoice_line_ids[0]
+        self.assertEqual(self.move_line.analytic_distribution, {f'{self.move_line.move_id.x_task_id.project_id.account_id.id}': 100.0})
 
     def test_add_default_analytic_account_server_action(self):
         self.move_line = self.demo_account_move.invoice_line_ids[0]
         self.move_line.analytic_distribution = False
+        # Check that the automation is not triggered when move_line changes
         self.assertEqual(self.move_line.analytic_distribution, False)
-        server_action = self.env['ir.actions.server'].search([('name', '=', 'Add default analytic account')])
-        server_action.with_context(active_id=self.move_line.id).run()
-        self.assertEqual(self.move_line.analytic_distribution, {self.move_line.move_id.x_task_id.project_id.account_id.id: 100})
+        server_action = self.env['ir.actions.server'].browse(self.env.ref('handyman.action_add_default_analytic_account').id)
+        server_action.with_context(active_id=self.move_line.id, active_model="account.move.line").run()
+        self.assertEqual(self.move_line.analytic_distribution, {f'{self.move_line.move_id.x_task_id.project_id.account_id.id}': 100.0})
+
+    def test_add_section_task_name_base_automation(self):
+        for project_task in self.sale_order_1.tasks_ids:
+            self.assertEqual(project_task.name[-12:], " - section_1")
 
     def test_add_section_task_name_server_action(self):
-        self.assertEqual(1, None)
+        for project_task in self.sale_order_1.tasks_ids:
+            project_task.name = "project_task_1"
+            # Check that the automation is not triggered when project task is changed
+            self.assertEqual(project_task.name, "project_task_1")
+            server_action = self.env['ir.actions.server'].browse(self.env.ref('handyman.action_add_section_task_name').id)
+            server_action.with_context(active_ids=[project_task.id], active_model="project.task").run()
+            self.assertEqual(project_task.name, "project_task_1 - section_1")
