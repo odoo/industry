287ebb3b54f77a80f3bc4ab06ef21bbfc51a5720
Dhrumil Shah <dhrs@odoo.com>
2025-05-16 06:01:15 +0000

    [ADD] florist: Florist
    Odoo for Florist
    
    - This setup is perfect for florists who specialize in providing fresh flowers,
      customized bouquets, and event floral designs, catering to walk-in customers
      and fulfilling orders for occasions like weddings, birthday dinners, or
      corporate events.
    
    - The CRM app helps you manage customer relationships, event requests, and
      flower inquiries easily.
    
    - The Sales app makes it simple to handle customer orders, including big floral
      arrangements for weddings, corporate events, and other special occasions.
    
    - The Purchase app manages vendor and ensures your shop is stocked with the
      freshest flowers.
    
    - The Point of Sale app handles in-store sales of bouquets, flowers, gift cards,
      and more.
    
    - The Website app allows customers to showcase your flowers vision and services
      online.
    
    Task-4757667
    
    closes odoo/industry#649
    
    closes odoo/industry#716
    
    X-original-commit: e6a03e26621fd957dd4f14c158cf187eb7c1f767
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

---
+++ florist/static/src/js/my_tour.js
@@ -0,0 +1,12 @@
+import { _t } from "@web/core/l10n/translation";
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("florist_knowledge_tour", {
+    steps: () => [
+        {
+            trigger: '.o_app[data-menu-xmlid="knowledge.knowledge_menu_root"]',
+            content: _t("Get on track and explore our recommendations for your Odoo usage here!"),
+            run: "click",
+        },
+    ],
+});
