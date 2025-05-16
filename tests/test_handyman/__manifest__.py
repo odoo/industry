fb4462d8f7261e28f4d820e3bd107dce5fe45666
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-16 17:31:03 +0200

    [FIX] handyman: correction of small reported issues

--- tests/test_handyman/__manifest__.py
+++ tests/test_handyman/__manifest__.py
@@ -1,9 +1,8 @@
 {
-    'name': 'test_handyman',
+    'name': 'Test Industry Handyman',
     'version': '1.0',
     'category': 'Hidden/Tests',
     'description': """A module to test code in the Handyman industry module.""",
     'depends': ['base'],
-    'installable': True,
     'license': 'LGPL-3',
 }
