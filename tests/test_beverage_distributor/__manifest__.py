10eee04546120b4844267e0c8e3f6d68f1d18f2d
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-19 15:36:41 +0200

    [FIX] tests: runbot failing tests error

---
+++ tests/test_beverage_distributor/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'test_beverage_distributor',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the Beverage distributor industry module.""",
+    'depends': ['base'],
+    'installable': True,
+    'license': 'LGPL-3',
+}
