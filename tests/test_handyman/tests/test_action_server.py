f302955bf2d4a6cbe9cc4837aff4559a4bc625b2
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-19 15:36:41 +0200

    [FIX] tests: runbot failing tests error

--- tests/test_handyman/tests/test_action_server.py
+++ tests/test_handyman/tests/test_action_server.py
@@ -1,9 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 from odoo import fields, Command
 
 
+@tagged('post_install', '-at_install')
 class ActionServerTestCase(TransactionCase):
 
     @classmethod
