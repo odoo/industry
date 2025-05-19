82a8934baf67961555c4c5e17ebcf78c9e926a47
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-19 16:53:01 +0200

    [FIX] tests: empty test beverage distributor folder removed

--- tests/test_beverage_distributor/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'test_beverage_distributor',
-    'version': '1.0',
-    'category': 'Hidden/Tests',
-    'description': """A module to test code in the Beverage distributor industry module.""",
-    'depends': ['base'],
-    'installable': True,
-    'license': 'LGPL-3',
-}
