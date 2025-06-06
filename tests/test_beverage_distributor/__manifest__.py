3e14d8fcd1859c4398c56b47daa49fba93a68f53
Francois Vasamillet (frva) <frva@odoo.com>
2025-06-06 10:01:36 +0000

    [IMP] tests: beverage distributor server action tests
    This commit adds tests to verify the good execution of server actions in the beverage_distributor module.
    
    TASK-4715882
    
    X-original-commit: 096e2ce791e04159b2a5e2e2f55ca1a440819288

---
+++ tests/test_beverage_distributor/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test beverage Distributor',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the Beverage distributor industry module.""",
+    'depends': ['base'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
