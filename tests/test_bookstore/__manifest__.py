557d005bd6b80495818e6ef3ee733691cf5b0e93
Arthur Mermet <amer@odoo.com>
2025-06-18 15:21:06 +0000

    [ADD] bookstore: tests on keys automations
    task-4715882
    
    closes odoo/industry#745
    
    X-original-commit: df0e03dc24ddb80366e915652846ade29eba6134
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

---
+++ tests/test_bookstore/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test Bookstore',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the bookstore industry.""",
+    'depends': ['base'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
