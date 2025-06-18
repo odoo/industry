c734ffec6335d4baf0be8fdeba7e817101ea3d12
vava-odoo <vava@odoo.com>
2025-06-18 15:37:19 +0200

    last (?) changes

--- excise_management/__manifest__.py
+++ excise_management/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Excise Management',
     'version': '1.0',
-    'category': 'Inventory/Inventory',
+    'category': 'Supply Chain/Inventory',
     'description': 'Define excises applying to specific products and leverage inventory reporting',
     'author': 'Odoo S.A.',
     'depends': [
@@ -21,11 +21,9 @@
         'data/ir_model_access.xml',
         'data/ir_default.xml',
         'data/account_tax_group.xml',
-        # 'data/account_tax.xml',
         'data/stock_warehouse.xml',
         'data/uom_uom.xml',
         'data/x_excise_category.xml',
-        'data/x_excise_category_line.xml',
         'data/res_config_setting.xml',
     ],
     'license': 'OPL-1',
