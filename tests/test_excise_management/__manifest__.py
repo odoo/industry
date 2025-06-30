4de27215529ca278f3606e0a3ab47113dfcf260a
Francois Vasamillet (frva) <frva@odoo.com>
2025-06-30 12:10:24 +0000

    [IMP] tests: excise management server action tests
    This commit adds tests to verify the good execution of server actions in the excise_management module.
    
    TASK-4715882
    
    X-original-commit: b6aabc43ab02b7769e11ddc54210870790cdf8be

---
+++ tests/test_excise_management/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test Excise Management',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the Excise Management module.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
