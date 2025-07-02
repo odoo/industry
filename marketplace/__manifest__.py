c5e5505ab601a1521baea78fa3286c36de4934e8
Francois Vasamillet (frva) <frva@odoo.com>
2025-07-02 16:02:18 +0200

    [IMP] marketplace: add res setting

---
+++ marketplace/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Marketplace',
+    'version': '1.0',
+    'category': 'Services',
+    'depends': [
+        'contacts',
+        'hr',
+        'knowledge',
+        'website',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/res_config_settings_views.xml',
+    ],
+    'license': 'OPL-1',
+    'author': 'Odoo S.A.',
+}
