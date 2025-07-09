93e9a04e38581a8e439a0313bbf6e6f1d214c21b
Arthur Mermet <amer@odoo.com>
2025-07-09 17:17:06 +0200

    [ADD] shoe_maker: tests on keys automations
    task-4715882

---
+++ tests/test_shoe_maker/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test Shoe Maker',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the shoe maker industry.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
