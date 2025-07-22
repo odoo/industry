41a9db6682d3c36986ac99abd974a1c6dc9f74bf
vava-odoo <vava@odoo.com>
2025-07-22 15:59:01 +0200

    [IMP] test_generic: allow not-industry modules

--- tests/test_generic/tests/industry_case.py
+++ tests/test_generic/tests/industry_case.py
@@ -4,6 +4,8 @@ import os
 
 from odoo.tests.common import TransactionCase
 
+CATEGORIES = ('Services', 'Retail', 'Construction', 'Hospitality', 'Health and Fitness', 'Supply Chain')
+
 
 def get_industry_path():
     return os.path.abspath(__file__).split('tests/test_generic/')[0]
@@ -23,6 +25,8 @@ class IndustryCase(TransactionCase):
 
     def setUp(cls):
         super().setUp()
-        cls.installed_modules = cls.env['ir.module.module'].search(
+        modules = cls.env['ir.module.module'].search(
             [('name', 'in', get_modules()), ('state', '=', 'installed')]
-        ).mapped('name')
+        )
+        cls.installed_modules = modules.mapped('name')
+        cls.installed_industries = modules.filtered(lambda m: m.category_id.name in CATEGORIES).mapped('name')
