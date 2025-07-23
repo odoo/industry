b394838e82b06cfc25dcbce7efc0a03916539627
Arthur Mermet <amer@odoo.com>
2025-07-23 15:44:37 +0200

    [ADD] return: sub app needed for library
    task-4965689

---
+++ return/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Return',
+    'version': '1.0',
+    'category': 'Inventory/Inventory',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'base_automation',
+        'point_of_sale',
+        'stock',
+        'web_studio',
+    ],
+    'data': [
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_ui_view.xml',
+    ],
+    'license': 'OPL-1',
+}
