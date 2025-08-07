2c2f1fb2ad89f8c0314f3f0754e328aacbfd4102
Francois Vasamillet (frva) <frva@odoo.com>
2025-08-07 11:38:09 +0200

    [IMP] logistic_company: adapt storage category, add fee rate

---
+++ logistic_company/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': '3PL Logistic',
+    'version': '1.0',
+    'depends': [
+        'base_industry_data',
+        'sale_management',
+        'stock',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/ir_model.xml',
+        'data/ir_model_access.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_ui_view.xml',
+    ],
+    'demo': [
+    ],
+    'license': 'OPL-1',
+    'author': 'Odoo S.A.',
+}
