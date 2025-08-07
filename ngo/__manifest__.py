18b9d1c0cdffbc13a805ee7a8123fe86e3ce6ca8
Francois Vasamillet (frva) <frva@odoo.com>
2025-08-07 13:51:59 +0200

    [ADD] ngo: add ngo module with base folders and files

---
+++ ngo/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Nonprofit organization',
+    'version': '2.0',
+    'category': 'Services',
+    'depends': [
+        'base_industry_data',
+        'knowledge',
+        'mass_mailing',
+        'sale_subscription',
+        'web_studio',
+        'website_crm',
+        'website_event_sale',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/res_partner_category.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_ui_menu.xml',
+        'data/base_automation.xml',
+        'data/ir_model_access.xml',
+    ],
+    'demo': [],
+    'author': 'Odoo S.A.',
+    'license': 'OPL-1'
+}
