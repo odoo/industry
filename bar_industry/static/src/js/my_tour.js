4e83a0d4ca1e21e4679954c6bd298ad99905adb4
mega <mega@odoo.com>
2025-08-29 07:31:41 +0000

    [IMP] bar_industry: improve master / demo data
    This commit adds new features for the bar_industry module, such as kiosk attendance, accept customer tips, POS rights for employees, price lists presets (Happy hour) and show product images, table booking.
    
    It also reworks data by adding employees, product's categories, pos category, products, partners, and shift templates.
    
    Replaced Paradise drink with First Company.
    
    TASK-4486017
    
    closes odoo/industry#1037
    
    Forward-port-of: odoo/industry#670
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- bar_industry/static/src/js/my_tour.js
+++ bar_industry/static/src/js/my_tour.js
@@ -4,7 +4,6 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("bar_industry_knowledge_tour", {
-    url: "/odoo",
     
     steps: () => [
         {
