1db31d4900562d555144868f135c87f8433d26a9
vava-odoo <vava@odoo.com>
2025-09-02 13:15:44 +0000

    [IMP] excise_management: handle sales taxes for fiscal deposits
    Before this commit, when selling a product with an excise tax to a
    partner with a fiscal position which is a fiscal deposit, the excise tax
    was added to the sale order line.
    
    This commit improves the feature by adding a replacement tax to the
    non-fiscal deposit fiscal positions, such that the excise tax is not
    taking into account anymore. The equivalent change is done on the
    reporting on stock.move. It also automatically handles new fiscal
    positions and new excise categories.
    
    task-5030707
    
    closes odoo/industry#1060
    
    X-original-commit: b7bdd7f72dbebfd6d4033a9c2d6f8ae5e290cc5d
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- excise_management/__manifest__.py
+++ excise_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Excise Management',
-    'version': '2.0',
+    'version': '2.1',
     'category': 'Inventory/Inventory',
     'author': 'Odoo S.A.',
     'depends': [
@@ -14,12 +14,12 @@
         'data/ir_model_fields.xml',
         'data/ir_ui_view.xml',
         'data/ir_actions_act_window.xml',
+        'data/account_tax_group.xml',
         'data/ir_actions_server.xml',
         'data/ir_ui_menu.xml',
         'data/base_automation.xml',
         'data/ir_model_access.xml',
         'data/ir_default.xml',
-        'data/account_tax_group.xml',
         'data/stock_warehouse.xml',
         'data/uom_uom.xml',
         'data/x_excise_category.xml',
