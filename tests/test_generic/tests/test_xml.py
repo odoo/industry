924b1d96fecd0cf1359cc0b62dbf03113f4bc6de
dhrs-odoo <dhrs@odoo.com>
2025-09-30 15:42:13 +0530

    [FIX] test_generic: fix test for relative dates

--- tests/test_generic/tests/test_xml.py
+++ tests/test_generic/tests/test_xml.py
@@ -489,6 +489,7 @@ class TestEnv(IndustryCase):
         RELATIVE_DATES = [
             'Time.',
             'time.',
+            'time(',
         ]
         for record in root.xpath("//record"):
             model_name = record.get('model')
