a557017742affdc39bf1eca01f732318ce93cff7
vava-odoo <vava@odoo.com>
2025-09-30 19:37:58 +0000

    [IMP] (test_)condominium: add acquisition tour
    X-original-commit: dc4688dbf9d2f2ffcb885aa950090238547af3a4

---
+++ tests/test_condominium/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'test condominium industry',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test Condominium business flows.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
