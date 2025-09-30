65ced71ba33a67855381b248d502227b19ee07c9
abdrahmanrashed <aras@odoo.com>
2025-09-30 14:14:43 +0200

    [IMP] utils: update run industry script.
    - set -r to reset only industry modules.
    - added -h to reset all modules.

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def get_zip(module_name, env):
     external_modules = get_external_dependencies('industry/' + module_name, env)
     dirs = ['industry/' + module_name] + ['industry/' + module for module in external_modules]
     for dir in dirs:
-        for dirname, subdirs, files in os.walk(dir):
+        for dirname, _, files in os.walk(dir):
             zip_dirname = os.sep.join(dirname.split(os.sep)[1:])
             zf.write(dirname, zip_dirname)
             for filename in files:
@@ -20,12 +20,22 @@ def get_zip(module_name, env):
     return output
 
 
-def get_external_dependencies(dir, env):
+def get_dependencies(dir):
     manifest_file = dir + '/__manifest__.py'
     if not os.path.exists(manifest_file):
         raise FileNotFoundError(f"Manifest file not found: {manifest_file}")
     with open(manifest_file, encoding="utf-8") as f:
         manifest = ast.literal_eval(f.read())
-    dependencies = set(manifest.get('depends', ['base']))
+    return set(manifest.get('depends', ['base']))
+
+
+def get_external_dependencies(dir, env):
+    dependencies = get_dependencies(dir)
     known_modules = env['ir.module.module'].search([]).mapped('name')
     return dependencies.difference(known_modules)
+
+
+def install_internal_dependencies(dir, env):
+    dependencies = get_dependencies(dir)
+    modules = env['ir.module.module'].search([('name', 'in', dependencies)])
+    return modules.button_immediate_install()
