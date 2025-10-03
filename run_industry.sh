b76e43f00429ccb49874c9cfc4093de436f0bef7
abdrahmanrashed <aras@odoo.com>
2025-10-03 13:10:26 +0000

    [IMP] utils: industry run script improvements.
    - swapped db dump logic for dp duplicate for increased preformance.
    - set [-i] to be optional and added -n for db name input
    
    closes odoo/industry#1195
    
    X-original-commit: 0421ee35090ea014b5bef12885cedca2c7385564
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- run_industry.sh
+++ run_industry.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 
 # Usage function
 usage() {
-  echo "Usage: ./industry/run_industry.sh -i <industry-name> [-d] [-t] [-r | -h]"
-  echo "  -i <industry-name>   (re)Install this industry"
+  echo "Usage: ./industry/run_industry.sh -n <industry-name> [-i] [-d] [-t] [-r | -h]"
+  echo "  -n <industry-name>   Name of the industry to run"
+  echo "  -i                   Import this industry"
   echo "  -d                   Enable demo data when installing"
   echo "  -t                   Run tests for the installed industry"
   echo "  -r                   Reset the database before running but keeps the industry dependencies installed"
@@ -21,9 +22,10 @@ HARD_RESET=false
 DEMO=False
 
 # Parse arguments
-while getopts ":i:dtrh" opt; do
+while getopts ":n:idtrh" opt; do
   case $opt in
-    i)  INDUSTRY_NAME="$OPTARG"; INSTALL=true ;;
+    n)  INDUSTRY_NAME="$OPTARG";;
+    i)  INSTALL=true ;;
     d)  DEMO=True ;;
     t)  TEST=true ;;
     r)  RESET=true ;;
@@ -43,19 +45,31 @@ PYTHON_BIN="python3"
 ODOO_BIN="odoo/odoo-bin"
 ADDONS_PATH="industry/tests,enterprise,odoo/addons,odoo/odoo/addons,design-themes"
 TEST_TAGS="/test_generic,/test_$INDUSTRY_NAME"
-DUMP_PATH=../dump"/$INDUSTRY_NAME"
+DEP_DB="dep-$INDUSTRY_NAME"
 
-# create dump if it doesn't exist
-if [[ ! -f "$DUMP_PATH" ]]; then
+#check module exists
+if [ ! -d "industry/$INDUSTRY_NAME" ]; then
+  echo "Module '$INDUSTRY_NAME' does not exist."
+  exit 1
+fi
+
+#check manifest exists
+if [ ! -f "industry/$INDUSTRY_NAME/__manifest__.py" ]; then
+  echo "Manifest file not found."
+  exit 1
+fi
+
+#try to init the db to check if it exists
+if $PYTHON_BIN $ODOO_BIN db init $DEP_DB >/dev/null 2>&1; then
   HARD_RESET=true
 fi
 
+# reset the dependency db
 if $HARD_RESET; then
-  echo "Resetting database '$INDUSTRY_NAME'..."
-  mkdir -p "$(dirname "$DUMP_PATH")"
+  echo "Resetting database '$DEP_DB'..."
+  $PYTHON_BIN $ODOO_BIN db init $DEP_DB --force
   echo "Initializing dependencies..."
-  $PYTHON_BIN $ODOO_BIN db init $INDUSTRY_NAME --force
-  $PYTHON_BIN $ODOO_BIN --addons-path="$ADDONS_PATH" module install base_import_module -d $INDUSTRY_NAME
+  $PYTHON_BIN $ODOO_BIN --addons-path="$ADDONS_PATH" module install base_import_module -d $DEP_DB
   TMP_DEP_PY=$(mktemp)
   cat <<EOF > "$TMP_DEP_PY"
 import sys
@@ -66,20 +80,19 @@ print("")
 env.cr.commit()
 exit()
 EOF
-  cat $TMP_DEP_PY | $PYTHON_BIN $ODOO_BIN shell --addons-path="$ADDONS_PATH" -d $INDUSTRY_NAME
+  cat $TMP_DEP_PY | $PYTHON_BIN $ODOO_BIN shell --addons-path="$ADDONS_PATH" -d $DEP_DB
   rm -f "$TMP_DEP_PY"
-  $PYTHON_BIN $ODOO_BIN db dump $INDUSTRY_NAME $DUMP_PATH
 fi
 
 # reload db when reset
 if $RESET || $HARD_RESET; then
-  $PYTHON_BIN $ODOO_BIN db load $INDUSTRY_NAME $DUMP_PATH --force
+  echo "Copying database '$DEP_DB' into '$INDUSTRY_NAME'..."
+  $PYTHON_BIN $ODOO_BIN db duplicate $DEP_DB $INDUSTRY_NAME --force
 fi
 
 # install industry module 
 if $INSTALL; then
   echo "Initializing industry module..."
-  
   TMP_INSTALL_PY=$(mktemp)
   cat <<EOF > "$TMP_INSTALL_PY"
 import sys
