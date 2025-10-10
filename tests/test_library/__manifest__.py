89aee267efebc9cd6b89c22a10321b91509960da
Tristan Cazier <trcaz@odoo.com>
2025-10-10 13:12:53 +0000

    [ADD] library: add tests
    This commit adds tests for computed logic in the library module, i.e. the server actions and automations (there are no computed fields).
    
    task-151564523
    
    closes odoo/industry#1227
    
    X-original-commit: 5e748a5e1fcbbaa17427478fb0dd3047e8a84dbb
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>
    Signed-off-by: Tristan Cazier (trcaz) <trcaz@odoo.com>

---
+++ tests/test_library/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test Library',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the industry library.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
