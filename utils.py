bae28d1d6f5af8c64a9c30c9f5b02193552d9594
abdrahmanrashed <aras@odoo.com>
2025-10-17 14:44:17 +0000

    [IMP] utils: unify zip signature between industry versions
    This commit aligns the get_zip signature across all versions, following the change in https://github.com/odoo/industry/commit/508e04e7d93dfa7d3605d4ec0811bd89b1951ce0
    
    closes odoo/industry#1242
    
    Forward-port-of: odoo/industry#1239
    Signed-off-by: Abdelrahman Rashed (aras) <aras@odoo.com>

--- utils.py
+++ utils.py
@@ -4,13 +4,17 @@ from io import BytesIO
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
-def get_zip(module_name, env):
-    output = BytesIO()
-    zf = ZipFile(output, "w", ZIP_DEFLATED)
-    for dirname, subdirs, files in os.walk('industry/' + module_name):
-        zip_dirname = os.sep.join(dirname.split(os.sep)[1:])
-        zf.write(dirname, zip_dirname)
-        for filename in files:
-            zf.write(os.path.join(dirname, filename), os.path.join(zip_dirname, filename))
-    zf.close()
-    return output
+class IndustryUtils:
+    def __init__(self, industry_path: str):
+        self.industry_path = industry_path
+
+    def get_zip(self, module_name: str):
+        output = BytesIO()
+        zf = ZipFile(output, "w", ZIP_DEFLATED)
+        for dirname, _, files in os.walk(self.industry_path + module_name):
+            zip_dirname = os.sep.join(dirname.split(os.sep)[1:])
+            zf.write(dirname, zip_dirname)
+            for filename in files:
+                zf.write(os.path.join(dirname, filename), os.path.join(zip_dirname, filename))
+        zf.close()
+        return output
