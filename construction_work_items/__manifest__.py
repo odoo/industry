be6ef10004ecc4965ed063a2796cc958afe3a7aa
Francois Vasamillet (frva) <frva@odoo.com>
2025-10-22 14:33:56 +0200

    [ADD] construction: add submodules and add the corresponding settings in construction

---
+++ construction_work_items/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Construction Work Items',
+    'version': '1.0',
+    'category': 'Construction',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'sale_management',
+        'web_studio',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+    ],
+    'demo': [
+
+    ],
+    'license': 'OPL-1',
+    'cloc_exclude': [],
+    'website': "https://www.odoo.com/all-industries",
+}
