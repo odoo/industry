ad7bb5706486670841ce4f4ff5cc3279dfa7f438
Francois Vasamillet (frva) <frva@odoo.com>
2025-10-22 14:33:56 +0200

    [ADD] construction: add submodules and add the corresponding settings in construction

---
+++ construction_line_numbering/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Construction Line Numbering',
+    'version': '1.0',
+    'category': 'Construction',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'sale_management',
+        'web_studio',
+    ],
+    'data': [
+    ],
+    'demo': [
+
+    ],
+    'license': 'OPL-1',
+    'cloc_exclude': [],
+    'website': "https://www.odoo.com/all-industries",
+}
