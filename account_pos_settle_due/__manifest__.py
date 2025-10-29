1d0ebd7cf6aef7e1d07a4a7f03a3f583028fdc0f
vava-odoo <vava@odoo.com>
2025-10-29 16:37:24 +0100

    [ADD] account_pos_settle_due: add module to invoice customer account
    This commit adds a new module to improve the customer experience by
    bridging the point of sale module with the invoicing one. With this
    module, it is now possible to settle all customer account from any point
    of sale and add them to an unified invoice listing all orders. When
    confirming the invoice, all customer accounts from these pos orders are
    reset.
    
    task-5142276

---
+++ account_pos_settle_due/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account - Point of Sale Settle Due',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'pos_settle_due',
+        'web_studio',
+    ],
+    'data': [
+        'data/ir_model_fields.xml',
+        'data/ir_actions_server.xml',
+        'data/base_automation.xml',
+        'data/ir_ui_view.xml',
+    ],
+    'license': 'OPL-1',
+}
