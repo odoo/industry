9829e5a32f181e59da9cf0e241469cec0031bec1
Francois Vasamillet (frva) <frva@odoo.com>
2025-10-29 16:28:04 +0100

    [ADD] customer_check_in: add base structure for module

---
+++ customer_check_in/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Customer Check In',
+    'version': '1.0',
+    'category': 'Hospitality',
+    'depends': [
+        'event',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'license': 'OPL-1',
+    'author': 'Odoo S.A.',
+    "cloc_exclude": [
+
+    ],
+    'images': [],
+}
