b27c4fb345466878eab03d48780980a9898780d7
Francois Vasamillet (frva) <frva@odoo.com>
2025-10-30 09:49:49 +0000

    [FIX] test_industry_real_estate: fix x_rental_contract_id test
    Before this commit, the test failed because of one sale order demo data because its end_date was "30-10-{current_year}", making the related account_analytic_account missing a valid x_rental_contract_id by computation.
    Instead of changing it, we complete the test taking into account the account_analytic_account that are not currently linked like this one to any sale order.
    The test execute like before if there is one, otherwise we check that the sale order is either finished or to be started in the future.
    
    X-original-commit: 13a1f91f720c7f15a5f4a09895af6d105555ea38

--- tests/test_industry_real_estate/tests/test_computed_fields.py
+++ tests/test_industry_real_estate/tests/test_computed_fields.py
@@ -8,14 +8,19 @@ class ComputedFieldsTestCase(TransactionCase):
 
     def test_x_rental_contract_id_computation(self):
         for sale_order in self.env['sale.order'].search([('x_account_analytic_account_id', '!=', False)]):
-            if sale_order.x_account_analytic_account_id:
-                aa = sale_order.x_account_analytic_account_id
+            aa = sale_order.x_account_analytic_account_id
+            if rental_contract := aa.x_rental_contract_id:
                 start_date = sale_order.x_rental_start_date
                 self.assertTrue(
-                    (sale_order == aa.x_rental_contract_id) or (
-                        aa == aa.x_rental_contract_id.x_account_analytic_account_id and (
-                            (start_date < aa.x_rental_contract_id.x_rental_start_date) or
+                    (sale_order == rental_contract) or (
+                        aa == rental_contract.x_account_analytic_account_id and (
+                            (start_date < rental_contract.x_rental_start_date) or
                             (start_date > datetime.date.today())
                         )
                     )
+                )
+            else:
+                self.assertTrue(
+                    sale_order.subscription_state in ['5_renewed', '6_churn']
+                    or sale_order.x_rental_start_date > datetime.date.today()
                 )
