9e176a0293a7ea2b3603c66db38758906fab2427
Francois Vasamillet (frva) <frva@odoo.com>
2025-10-30 09:43:31 +0100

    [IMP] customer_check_in: Add app menus and modify event view

--- customer_check_in/__manifest__.py
+++ customer_check_in/__manifest__.py
@@ -3,10 +3,16 @@
     'version': '1.0',
     'category': 'Hospitality',
     'depends': [
-        'event',
+        'base_industry_data',
+        'event_sale',
+        'knowledge',
+        'website_event',
     ],
     'data': [
-
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_ui_menu.xml',
     ],
     'demo': [
 
