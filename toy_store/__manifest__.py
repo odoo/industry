e7313068cdf242842450bc96a6d7b86fb45d3148
amas-odoo <amas@odoo.com>
2025-11-10 17:25:23 +0530

    [FIX] global: Prevent mail box pollution when installing an industry
    Part 5:
    When an industry module is installed, unnecessary mail records were being
    created. This change ensures the mailbox remains clean during installation.
    
    Task ID: 4671943

--- toy_store/__manifest__.py
+++ toy_store/__manifest__.py
@@ -33,6 +33,7 @@
         'data/knowledge_tour.xml',
     ],
     'demo': [
+        'demo/res_users.xml',
         'demo/res_partner.xml',
         'demo/hr_employee.xml',
         'demo/product_template.xml',
