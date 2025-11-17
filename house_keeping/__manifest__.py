fd352a00be96c51c4dc11dc7a90eca5a8fccd74c
Francois Vasamillet (frva) <frva@odoo.com>
2025-11-17 09:57:14 +0100

    [IMP] house_keeping: add menu data

---
+++ house_keeping/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'House Keeping',
+    'version': '1.0',
+    'category': 'Hidden/Tools',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'booking_engine',
+    ],
+    'data': [
+        'data/ir_ui_menu.xml',
+    ],
+    'demo': [
+    ],
+    'license': 'OPL-1',
+    'cloc_exclude': [
+    ],
+    'website': "https://www.odoo.com/all-industries",
+}
