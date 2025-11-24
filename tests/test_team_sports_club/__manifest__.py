6af60f955d71f04bec76180a7fd2030a9ce3557b
amas-odoo <amas@odoo.com>
2025-11-24 17:48:12 +0530

    [ADD] team_sports_club: test for server actions and computed fields
    Validates:
    - UserError raised when no partner selected
    - Sale order creation and confirmation
    - Correct product on order line
    - Partner tags successfully assigned
    
    closes odoo/industry#1401
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

---
+++ tests/test_team_sports_club/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test Team Sports Club',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the team sports club industry.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
