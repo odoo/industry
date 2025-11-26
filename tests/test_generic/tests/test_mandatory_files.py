b80ce982f9cbd9f028f7dd399215ab17a519c51a
Francois Vasamillet (frva) <frva@odoo.com>
2025-11-26 12:31:01 +0100

    [IMP] test_generic: modify test for modules without translation needed
    Change test_required_files method to raise a warning when the module doesn't need translation but
    - There is a pot file
    - The module is in the weblate
    
    closes odoo/industry#1410
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- tests/test_generic/tests/test_mandatory_files.py
+++ tests/test_generic/tests/test_mandatory_files.py
@@ -4,8 +4,10 @@ import logging
 import os
 from pathlib import Path
 import re
+import io
 
 from odoo.tests import tagged
+from odoo.tools.translate import trans_export
 
 from .industry_case import IndustryCase, get_industry_path
 
@@ -17,11 +19,20 @@ class FileTest(IndustryCase):
 
     def test_required_files(self):
         for module in self.installed_modules:
+            needs_translation = False
+            with io.BytesIO() as buffer:
+                trans_export(False, [module], buffer, 'po', self.env)
+                if f"module: {module}" in buffer.getvalue().decode("utf-8"):
+                    needs_translation = True
+
             required_files = {
                 'index html file': '/static/description/index.html',
                 'init': '/__init__.py',
-                'pot file': f'/i18n/{module}.pot',
             }
+            if needs_translation:
+                required_files.update({
+                    'pot file': f'/i18n/{module}.pot',
+                })
             if module in self.installed_industries:
                 required_files.update({
                     'icon': '/static/description/icon.png',
@@ -31,7 +42,13 @@ class FileTest(IndustryCase):
                 if not os.path.isfile(get_industry_path() + module + path):
                     _logger.warning("Missing %s at %s", f, module + path)
 
+            if not needs_translation and os.path.isfile(get_industry_path() + module + f'/i18n/{module}.pot'):
+                _logger.warning("Remove pot file in the module at %s", module + f'/i18n/{module}.pot')
+
             weblate_config = Path(get_industry_path() + '.weblate.json').read_text(encoding="utf-8")
-            if not re.search(module, weblate_config):
+            if not (module_in_weblate := re.search(module, weblate_config)) and needs_translation:
                 _logger.error("Missing module in .weblate.json")
+                continue
+            elif module_in_weblate and not needs_translation:
+                _logger.warning("Remove module in .weblate.json")
                 continue
