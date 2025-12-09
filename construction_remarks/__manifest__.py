6085de06b9305ace89cbf507e9aadd747a2c6e87
Tristan Cazier <trcaz@odoo.com>
2025-12-09 12:11:50 +0100

    [ADD] construction_remarks: add new construction submodule
    This commit is part of a 2-part task (task-5148878). This commit adds the module, and the other updates the knowledge article and the setting to enable this submodule.
    
    This commit adds a new construction submodule, enabling the user to create remarks for projects, projects updates, and meetings (calendar.event), a menu to access them, and a button in the project top bar to access the project's remarks.
    
    task-5358871

---
+++ construction_remarks/__manifest__.py
@@ -0,0 +1,36 @@
+{
+    'name': 'Construction Remarks',
+    'version': '1.0',
+    'category': 'Hidden/Tools',
+    'depends': [
+        'base_industry_data',
+        'construction',
+        'project',
+        'web_studio',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_access.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_server.xml',
+        'data/base_automation.xml',
+        'data/ir_default.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_embedded_actions.xml',
+        'data/project_project.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'construction_remarks/static/src/widgets/project_remark_x2many_media_viewer.js',
+            'construction_remarks/static/src/widgets/project_remark_state_selection.js',
+        ],
+    },
+    'cloc_exclude': [
+        "static/src/widgets/project_remark_x2many_media_viewer.js",
+        "static/src/widgets/project_remark_state_selection.js",
+    ],
+    'license': 'OEEL-1',
+    'author': 'Odoo S.A.',
+}
