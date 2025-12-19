1c9ae128ee0ee605183e090024a7e5e02af8b43a
chga-odoo <chga@odoo.com>
2025-12-19 18:17:03 +0530

    [ADD] product_conversion: Multiple industries module
    - Take full control of inventory transitions. This module enables you to
      reclassify products and adjust grades seamlessly, ensuring total traceability
      for every Lot and Serial Number.
    - Effortless Downgrading: Instantly reassign a Lot from "Grade 1" to "Grade 2"
      during inventory adjustments while maintaining a comprehensive audit trail.
    - Product Adaptation: Pivot quickly by triggering a product change or
      reclassification immediately following a quality inspection.
    - Post-Production Flexibility: If output no longer meets its primary purpose,
      you can convert units into a secondary product—even after the production order
      is closed.
    
    Task- 5362405

---
+++ product_conversion/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Product Conversion & Grading Management',
+    'version': '1.0',
+    'category': 'Inventory',
+    'depends': [
+        'mrp',
+        'web_studio',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/ir_model.xml',
+        'data/ir_model_access.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_ui_view.xml',
+    ],
+    'demo': [
+        'demo/res_users.xml',
+    ],
+    'license': 'OEEL-1',
+    'images': ['images/main.png'],
+    'author': 'Odoo S.A.',
+}
