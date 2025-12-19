8161d1f366de661aab7b2060e4611a6744b7e55c
chga-odoo <chga@odoo.com>
2025-12-19 18:17:03 +0530

    [ADD] convert_mo: add module

---
+++ convert_mo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Convert MO',
+    'version': '1.0',
+    'category': 'Conversion',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/ir_model.xml',
+        'data/ir_model_access.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_ui_view.xml',
+    ],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
