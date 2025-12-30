0abb2a05b7632e8cefb24c3e19177631b74df4cd
Tristan Cazier <trcaz@odoo.com>
2025-12-30 13:03:05 +0100

    [FIX] construction_remarks: various fixes
    - Add description tab to remark form
    - Add a quick create view for remark kanban
    - Add remark kanban
    - Fix top bar not showing
    - Fix embedded action buttons not functioning properly
    - Try a fix for the group_expand

--- construction_remarks/static/src/js/relational_model.js
+++ construction_remarks/static/src/js/relational_model.js
@@ -3,22 +3,20 @@
 import { patch } from "@web/core/utils/patch";
 import { RelationalModel } from "@web/model/relational_model/relational_model";
 
-patch(RelationalModel.prototype, {   
-    /**
-     * Filter out non-remark stages when grouping by x_stage_id
-     * @param {*} config 
-     * @param {*} cache 
-     * @returns 
-     */ 
-    async _webReadGroup(config, cache) {
+patch(RelationalModel.prototype, {
+    async _webReadGroup(config, cache) { // Filter out non-remark stages when grouping by x_stage_id
         const result = await super._webReadGroup(config, cache);
         // filter out non-remark stages if grouping by x_stage_id
-        // because multiple groupbys are possible, we need to check if x_stage_id is the current groupby
-        if (config.groupBy.includes("x_stage_id") && result.groups.length > 0 && result.groups[0].x_stage_id) {
-            // we need the x_is_remark_stage field value
-            const stageIds = await (cache ? this.orm.cache(cache) : this.orm).search("project.task.type", [['x_is_remark_stage', '=', true]])
-            // keep only the remark stages of the result groups
-            result.groups = result.groups.filter((group) => stageIds.includes(group.x_stage_id[0]));
+        // because multiple nested groupbys are possible, we need to check if x_stage_id is the current groupby
+        if (config.resModel == "project.task" && result.groups.length > 0 && (config.groupBy.includes("x_stage_id") || config.groupBy.includes("stage_id"))) {
+            const stageIdList = (await this.env.services.orm.searchRead("project.task.type", [['x_is_remark_stage', '=', true]], ['id'])).map(stage => stage.id);
+            if (config.groupBy.includes("x_stage_id") && result.groups[0].x_stage_id) {
+                result.groups = result.groups.filter((group) => stageIdList.includes(group.x_stage_id[0]));
+            } else if (config.groupBy.includes("stage_id") && result.groups[0].stage_id) {
+                result.groups = result.groups.filter((group) => !stageIdList.includes(group.stage_id[0]));
+            }
+            result.length = result.groups.length;
+            await new Promise(resolve => setTimeout(resolve, 300));
         }
         return result;
     }
