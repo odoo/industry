f127a7133971772964704112bf336b6b679b672a
abdrahmanrashed <aras@odoo.com>
2026-01-02 09:53:24 +0000

    [FIX] */__manifest__: remove default version from modules
    - Removing `version: 1.0` since it is now implicitly set as default: https://github.com/odoo/odoo/commit/717619571d1297d6b299b7c47b728841bcd81e69
    
    X-original-commit: 241b741cccf7323cb2a139fea725864d92aefadc
    Part-of: odoo/industry#1483
    Signed-off-by: Abdelrahman Rashed (aras) <aras@odoo.com>

--- tests/test_team_sports_club/__manifest__.py
+++ tests/test_team_sports_club/__manifest__.py
@@ -1,6 +1,5 @@
 {
     'name': 'Test Team Sports Club',
-    'version': '1.0',
     'category': 'Hidden/Tests',
     'description': """A module to test code in the team sports club industry.""",
     'depends': ['base'],
