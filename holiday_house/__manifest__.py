ec88e4530eabc7fa3ebc35e40fd8d3f67ba559d0
abdrahmanrashed <aras@odoo.com>
2026-01-02 14:29:55 +0000

    [FIX] *: fix remaining modules on 19.1
    - Removed the remaining `product.templated.attribute.value` models that weren't removed in: https://github.com/odoo/industry/pull/1473
    
    -  Modified `real_estate` view according to changes in: https://github.com/odoo/odoo/commit/6ffc28f55f068cb7229e1873642fb53f1c33ef59
    
    -  Modified `real_estate` website template according to changes in: https://github.com/odoo/odoo/commit/f4aa56fd492de4c266b8107446b9ba2d78694faf
    
    X-original-commit: e96f0ab525166e4fe99d60cf54836ffadf82ce2d

--- holiday_house/__manifest__.py
+++ holiday_house/__manifest__.py
@@ -13,7 +13,6 @@
         'data/resource_resource.xml',
         'data/product_template.xml',
         'data/product_template_attribute_line.xml',
-        'data/product_template_attribute_value.xml',
         'data/product_image.xml',
         'data/product_product.xml',
         'data/knowledge_article.xml',
