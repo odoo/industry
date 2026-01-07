8d6d21fd40a5e935391afd2a731a6d1623412166
Xavier-Do <xdo@odoo.com>
2026-01-07 16:01:09 +0100

    [ADD] test_industry: add industry subcommand
    Part-of: odoo/industry#1762
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- utils.py
+++ utils.py
@@ -1,18 +1,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import os
+import pathlib
 from io import BytesIO
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
 class IndustryUtils:
-    def __init__(self, industry_path: str):
-        self.industry_path = industry_path
+    def __init__(self):
+        self.industry_path = pathlib.Path(__file__).parent
 
     def get_zip(self, module_name: str):
         output = BytesIO()
         zf = ZipFile(output, "w", ZIP_DEFLATED)
-        for dirname, _, files in os.walk(self.industry_path + module_name):
-            zip_dirname = os.sep.join(dirname.split(os.sep)[1:])
+        for dirname, _, files in os.walk(self.industry_path / module_name):
+            zip_dirname = os.sep.join(dirname.split(os.sep)[len(self.industry_path.parts):])
             zf.write(dirname, zip_dirname)
             for filename in files:
                 zf.write(os.path.join(dirname, filename), os.path.join(zip_dirname, filename))
