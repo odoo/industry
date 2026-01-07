7f15b3b45abebe8a55308afc02cdb687b3f4168b
Xavier-Do <xdo@odoo.com>
2026-01-07 16:01:09 +0100

    [ADD] test_industry: add industry subcommand

--- tests/test_condominium/tests/test_acquisition.py
+++ tests/test_condominium/tests/test_acquisition.py
@@ -1,11 +1,10 @@
-from odoo.tests import HttpCase, tagged, get_db_name
+from odoo.tests import HttpCase, tagged
 
 
 @tagged('post_install', '-at_install')
 class TestUi(HttpCase):
 
     def test_condominium_acquisition(self):
-        db_name = get_db_name()
-        if db_name.endswith('imported_no_demo'):
+        if not self.env['ir.module.module'].search_count([('demo', '=', True)], limit=1):
             return
         self.start_tour("/odoo", 'Condominium_Acquisition', login="admin")
