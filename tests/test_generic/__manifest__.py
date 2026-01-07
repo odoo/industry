b3b570bdbe15490e749657466d16d5f11f6d77bd
Xavier-Do <xdo@odoo.com>
2026-01-07 16:01:09 +0100

    [ADD] test_industry: add industry subcommand
    Part-of: odoo/industry#1766
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- tests/test_generic/__manifest__.py
+++ tests/test_generic/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'test',
+    'name': 'Generic industry tests',
     'version': '1.0',
     'category': 'Hidden/Tests',
     'description': """A module to test Odoo code with various linters.""",
