f333d049126b307079c8977f003d2791300f3a0a
Tristan Cazier <trcaz@odoo.com>
2026-01-09 12:29:04 +0100

    [IMP] construction_remarks: various stage-related fixes & imps
    - remove default x_stage_id everywhere. Instead, adds logic to pick one from the project's remark stages on write if no stage is selected
    - finally fix the wrong stages appearing in task & remark kanban each time a new remark is created
    - add the "Add Stage" button on the right of the kanban to quick create remark stages from the kanban
    - move JS patches from separate files to one to save lines

--- construction_remarks/static/src/js/kanban_controller.js
+++
@@ -1,9 +0,0 @@
-import { KanbanController } from "@web/views/kanban/kanban_controller";
-import { patch } from "@web/core/utils/patch";
-
-patch(KanbanController.prototype, {
-    async openRecord(record, { newWindow } = {}) { // Opens the remark form view when clicking on a remark kanban card instead of the default kanban behavior.
-        if (record.data.x_is_remark) {
-            await this.actionService.doAction("construction_remarks.action_x_remark_form_view", { additionalContext: { active_id: record.resId }, props: { resId: record.resId }});
-            return;}
-        return super.openRecord(record, { newWindow }); }});
---
+++ construction_remarks/static/src/js/patch.js
@@ -0,0 +1,52 @@
+import { KanbanController } from "@web/views/kanban/kanban_controller";
+import { patch } from "@web/core/utils/patch";
+import { CustomMediaDialog } from "@html_editor/fields/x2many_field/custom_media_dialog";
+import { X2ManyImageField, x2ManyImageField } from "@html_editor/fields/x2many_field/x2many_image_field";
+import { RelationalModel } from "@web/model/relational_model/relational_model";
+import { ProjectTaskKanbanDynamicGroupList } from "@project/views/project_task_kanban/project_task_kanban_model";
+
+// Opens the remark form view when clicking on a remark kanban card instead of the default task view
+patch(KanbanController.prototype, {
+    async openRecord(record, { newWindow } = {}) { // Opens the remark form view when clicking on a remark kanban card instead of the default kanban behavior.
+        if (record.data.x_is_remark) {
+            return this.actionService.doAction("construction_remarks.action_x_remark_form_view", { additionalContext: { active_id: record.resId }, props: { resId: record.resId }});}
+        return super.openRecord(record, { newWindow }); }});
+
+// Override isGroupedByStage to include x_stage_id to be able to quick create stages
+patch(ProjectTaskKanbanDynamicGroupList.prototype, { get isGroupedByStage() { return !!this.groupByField && ["stage_id", "x_stage_id"].includes(this.groupByField.name); }});
+
+// Filter out non-remark stages when grouping by x_stage_id (either in search view or kanban stages)
+patch(RelationalModel.prototype, {
+    async _postprocessReadGroup(config, { groups, length }) {
+        let { groups: res_groups, length: res_length } = await super._postprocessReadGroup(config, { groups, length });
+        // filter out non-remark stages if grouping by x_stage_id
+        // because multiple nested groupbys are possible, we need to check if x_stage_id is the current groupby
+        if (config.resModel == "project.task" && groups.length > 0  && res_groups.length > 0 && (config.groupBy.includes("x_stage_id") || config.groupBy.includes("stage_id"))) {
+            const stageIdsProjectsList = (await this.env.services.orm.searchRead("project.task.type", [['x_is_remark_stage', '=', true]], ['id', 'project_ids']));
+            if (config.groupBy.includes("x_stage_id") && groups[0].x_stage_id) {
+                res_groups = res_groups.filter((group) => stageIdsProjectsList.some((stage) => stage.id === group.value && stage.project_ids.includes(config.context.default_project_id)));
+            } else if (config.groupBy.includes("stage_id") && groups[0].stage_id) {
+                res_groups = res_groups.filter((group) => !stageIdsProjectsList.some((stage) => stage.id === group.value));}
+            res_length = res_groups.length;}
+        return { groups: res_groups, length: res_length };}});
+
+// Patch x2many image field to handle remark images specifically
+patch(X2ManyImageField.prototype, {  // Patching the instance
+    onFileRemove() {  // parentField not defined in our case, so we handle x_remark_image specifically
+        if (this.props.record.resModel === "x_remark_image") {
+            this.props.record._parentRecord.data["x_remark_image_ids"].delete(this.props.record);
+        } else { super.onFileRemove(); }},
+    onFileEdit(ev) {  // Overwriting to handle x_remark_image model specifically to hide video tab
+        if (this.props.record.resModel === "x_remark_image") {
+            console.log("found props:", this.props);
+            this.dialog.add(CustomMediaDialog, { visibleTabs: ["IMAGES"], activeTab: "IMAGES",
+                save: (el) => {},
+                imageSave: this.onImageSave.bind(this), videoSave: this.onVideoSave.bind(this)});
+        } else { super.onFileEdit(...arguments); }},
+    async onImageSave(attachment) { // Copy original logic, only change the field 'name' to 'x_name'
+        if (this.props.record.resModel === "x_remark_image") {
+            const attachmentRecord = await this.orm.searchRead("ir.attachment", [["id", "=", attachment[0].id]], ["id", "datas", "name"], {});
+            if (!attachmentRecord[0].datas) {
+                return this.notification.add(`Cannot add URL type attachment "${attachmentRecord[0].name}". Please try to reupload this image.`, {type: "warning",}); }
+            await this.props.record.update({[this.props.name]: attachmentRecord[0].datas, x_name: attachmentRecord[0].name,});
+        } else { super.onImageSave(...arguments); }}});
