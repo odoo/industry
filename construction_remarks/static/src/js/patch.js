1427397f6b757eff894dc2c1bbde5d10d79d1765
Tristan Cazier <trcaz@odoo.com>
2026-01-12 17:38:20 +0100

    [IMP] construction_remarks: savp's review 3
    Entirely replace the remark image model with the chatter attachments
    
    - opens the attachment view by default on the remark form view to allow for easy attachment of remark pictures
    - completely removed the remark image model, fields, defaults & view
    - in the image form, completely removed JS & CSS associated with the image field & its widget

--- construction_remarks/static/src/js/patch.js
+++ construction_remarks/static/src/js/patch.js
@@ -1,7 +1,5 @@
 import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { patch } from "@web/core/utils/patch";
-import { CustomMediaDialog } from "@html_editor/fields/x2many_field/custom_media_dialog";
-import { X2ManyImageField, x2ManyImageField } from "@html_editor/fields/x2many_field/x2many_image_field";
 import { RelationalModel } from "@web/model/relational_model/relational_model";
 import { ProjectTaskKanbanDynamicGroupList } from "@project/views/project_task_kanban/project_task_kanban_model";
 
@@ -29,24 +27,3 @@ patch(RelationalModel.prototype, {
                 res_groups = res_groups.filter((group) => !stageIdsProjectsList.some((stage) => stage.id === group.value));}
             res_length = res_groups.length;}
         return { groups: res_groups, length: res_length };}});
-
-// Patch x2many image field to handle remark images specifically
-patch(X2ManyImageField.prototype, {  // Patching the instance
-    onFileRemove() {  // parentField not defined in our case, so we handle x_remark_image specifically
-        if (this.props.record.resModel === "x_remark_image") {
-            this.props.record._parentRecord.data["x_remark_image_ids"].delete(this.props.record);
-        } else { super.onFileRemove(); }},
-    onFileEdit(ev) {  // Overwriting to handle x_remark_image model specifically to hide video tab
-        if (this.props.record.resModel === "x_remark_image") {
-            console.log("found props:", this.props);
-            this.dialog.add(CustomMediaDialog, { visibleTabs: ["IMAGES"], activeTab: "IMAGES",
-                save: (el) => {},
-                imageSave: this.onImageSave.bind(this), videoSave: this.onVideoSave.bind(this)});
-        } else { super.onFileEdit(...arguments); }},
-    async onImageSave(attachment) { // Copy original logic, only change the field 'name' to 'x_name'
-        if (this.props.record.resModel === "x_remark_image") {
-            const attachmentRecord = await this.orm.searchRead("ir.attachment", [["id", "=", attachment[0].id]], ["id", "datas", "name"], {});
-            if (!attachmentRecord[0].datas) {
-                return this.notification.add(`Cannot add URL type attachment "${attachmentRecord[0].name}". Please try to reupload this image.`, {type: "warning",}); }
-            await this.props.record.update({[this.props.name]: attachmentRecord[0].datas, x_name: attachmentRecord[0].name,});
-        } else { super.onImageSave(...arguments); }}});
