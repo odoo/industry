cfe63941f42a3a141a4902c2385ab183d3d5c5dc
Francois Vasamillet (frva) <frva@odoo.com>
2026-01-20 10:47:00 +0000

    [ADD] usage_based_maintenance: create module from machine_tool_rental
    This commit is moving the Usage Based Maintenance features from machine_tool_rental to a new dedicated module so it can be used for other modules later on.
    
    Task-5478090
    
    X-original-commit: 6995646c4fd2e89d44ab7867595a1bafeda5849e

---
+++ usage_based_maintenance/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Usage Based Maintenance',
+    'category': 'Hidden/Tools',
+    'author': 'Odoo S.A.',
+    'depends': [
+        'industry_fsm_sale_report',
+        'purchase',
+        'quality_control',
+        'sale_stock_renting',
+        'web_studio',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_model_access.xml',
+        'data/ir_default.xml',
+        'data/base_automation.xml',
+    ],
+    'demo': [
+        'demo/res_users.xml',
+    ],
+    'license': 'OEEL-1',
+    'images': ['images/main.png'],
+    'website': "https://www.odoo.com/all-industries",
+}
