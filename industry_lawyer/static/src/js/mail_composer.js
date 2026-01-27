e1de164bc41e7d483f3ac788bbd162479a70159c
mega-odoo <mega@odoo.com>
2026-01-27 18:52:43 +0530

    [IMP] industry_lawyer: cl-5

--- industry_lawyer/static/src/js/mail_composer.js
+++ industry_lawyer/static/src/js/mail_composer.js
@@ -8,7 +8,11 @@ import { user } from "@web/core/user";
 patch(Composer.prototype, {
     setup() {
         super.setup();
-
+        const composer = this.props.composer;
+        const thread = composer?.thread;
+        if (thread && ["sale.order", "account.move", "crm.lead"].includes(thread.model)) {
+            this.state.isVisible = true;
+        }
         onWillStart(async () => {
             this.state.isHighGroup = await user.hasGroup(
                 "industry_lawyer.group_high"
