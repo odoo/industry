6cce9ee40f37112c14002a8e48764596c82cdaf1
amas-odoo <amas@odoo.com>
2026-01-30 18:09:28 +0530

    [FIX] *: Final testing

--- tests/test_generic/tests/test_industry_mail_behavior.py
+++ tests/test_generic/tests/test_industry_mail_behavior.py
@@ -42,44 +42,48 @@ EXCEPTION_MODELS = [
 class IndustryMailBehaviorTestCase(IndustryCase):
 
     def test_generic_mail_message_generated(self):
-        messages = self.env['mail.mail']._read_group([('model', '!=', False), ('model', 'not in', EXCEPTION_MODELS), ('state', '!=', 'outgoing')], ['res_id', 'model'], ['id:count'])
-        
+        messages = self.env['mail.mail'].search_read([('model', '!=', False), ('model', 'not in', EXCEPTION_MODELS), ('state', '!=', 'outgoing')], ['model', 'res_id'])
+
         result = {}
-        for res_id, res_model, count in messages:
-            record = self.env[res_model].browse(res_id)
+
+        for msg in messages:
+            record = self.env[msg['model']].browse(msg['res_id'])
             if not record.exists():
                 continue
-            all_activities = getattr(record, 'activity_ids', False) or self.env['mail.activity']
-            upsell_activity = all_activities.filtered(lambda a: a.note and 'upsell' in a.note.lower())
-            if count == len(upsell_activity):
-                continue
+
+            if msg['model'] == 'res.partner':
+                try:
+                    is_any_subscription = self.env['sale.order'].search_count([('is_subscription', '=', True)])
+                except Exception:
+                    is_any_subscription = 0
+                if is_any_subscription > 0:
+                    _logger.warning(
+                            "Messages or mails were generated due to a subscription-type sale order. — To prevent message and mail pollution, use context `{'mail_auto_subscribe_no_notify': True, 'mail_notrack': True}` when confirming subscriptions or sale order."
+                        )
+                    continue
 
             ext_ids = record.get_external_id()
-            if not any(ext_ids.values()):
-                continue
 
             for ext_id in ext_ids.values():
-                ctxs = RECORD_CONTEXT_MODELS_DICT.get(res_model)
+                ctxs = RECORD_CONTEXT_MODELS_DICT.get(msg.get('model'))
                 if ctxs:
                     result.setdefault(ext_id, set()).update(ctxs)
 
-                if all_activities:
-                    for activity in (all_activities - upsell_activity):
+                if getattr(record, 'activity_ids', False):
+                    for activity in record.activity_ids.filtered(lambda a: 'Upsell' not in (a.note or '')):
                         activity_ext_ids = activity.get_external_id()
                         if not any(activity_ext_ids.values()):
                             result.setdefault(ext_id, set()).add('mail_activity_quick_update')
-                        else:
-                            for activity_ext_id in activity_ext_ids.values():
-                                result.setdefault(activity_ext_id, set()).add('mail_activity_quick_update')
+                        for activity_ext_id in activity_ext_ids.values():
+                            result.setdefault(activity_ext_id, set()).add('mail_activity_quick_update')
 
                 if getattr(record, 'calendar_event_ids', False):
                     for event in record.calendar_event_ids:
                         cal_ext_ids = event.get_external_id()
                         if not any(cal_ext_ids.values()):
                             result.setdefault(ext_id, set()).add('mail_activity_quick_update')
-                        else:
-                            for cal_ext_id in cal_ext_ids.values():
-                                result.setdefault(cal_ext_id, set()).add('mail_activity_quick_update')
+                        for cal_ext_id in cal_ext_ids.values():
+                            result.setdefault(cal_ext_id, set()).add('mail_activity_quick_update')
 
         for ext_ids, context in result.items():
             _logger.warning(
