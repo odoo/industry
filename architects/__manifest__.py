21a8d88373a79fbc7c951b903592be1e4fbc38b1
amas-odoo <amas@odoo.com>
2026-03-10 12:26:59 +0000

    [IMP] *: Clean industry customizations on module uninstall
    Uninstalling an industry module (e.g., real_estate) could leave modified
    window actions (domain, context, etc.) referencing fields created by the
    industry. After removal, these fields no longer exist, leading to tracebacks
    when opening related menus such as CRM or Products.
    
    Introduce an automation triggered when a module enters the to remove
    state to act similarly to an uninstall_hook. This ensures that industry
    customizations are properly cleaned during module removal.
    
    Task ID: 5936090
    
    X-original-commit: 307b8ad903f5335316012dfea569357019ae2a05

--- architects/__manifest__.py
+++ architects/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Architecture Firm',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'Services',
     'depends': [
         'account_followup',
@@ -45,6 +45,7 @@
         'data/website_view.xml',
         'data/website_theme_apply.xml',
         'data/knowledge_tour.xml',
+        'data/uninstall_hook.xml',
     ],
     'demo': [
         'demo/res_users.xml',
@@ -70,6 +71,7 @@
     'author': 'Odoo S.A.',
     "cloc_exclude": [
         "data/knowledge_article.xml",
+        "data/uninstall_hook.xml",
         "static/src/js/my_tour.js",
         "data/website_view.xml",
         "demo/website_view.xml",
