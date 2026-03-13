dcc33f74b0aab86ae74f66474c84b32f8a1d0200
mega-odoo <mega@odoo.com>
2026-03-13 18:05:27 +0530

    [IMP] industry_lawyer: cl-29 reload.js

--- industry_lawyer/static/src/js/reload.js
+++ industry_lawyer/static/src/js/reload.js
@@ -1,14 +1,17 @@
+import { user } from "@web/core/user";
 import { patch } from "@web/core/utils/patch";
 import { FormController } from "@web/views/form/form_controller";
 
 patch(FormController.prototype, {
-    async saveButtonClicked(params) {
-        const record = this.model.root;
-        const groupChanged = record?._changes?.group_ids;
-        const res = await super.saveButtonClicked(params);
-        if (this.props.resModel === "res.users" && groupChanged) {
-            window.location.reload();
-        }
-        return res;
-    },
+   async saveButtonClicked(params) {
+       const record = this.model.root;
+       const isUser = this.props.resModel === "res.users";
+       const groupChanged = isUser && !!record?._changes?.group_ids;
+       const isCurrentUser = isUser && record?.resId === user.userId;
+       const res = await super.saveButtonClicked(params);
+       if (isCurrentUser && groupChanged) {
+           window.location.reload();
+       }
+       return res;
+   },
 });
