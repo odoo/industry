f2d85a70b078091a8fd5b426c8b6498296ffe62e
Tristan Cazier <trcaz@odoo.com>
2026-03-18 11:16:57 +0000

    [IMP] construction{_developer}: add cost nature anaysis & improve data
    This commit adds a new view to compute the margin of products/work
    items/sale order lines on projects and sale orders using an SQL view
    through a server action.
    This commit also heavily reworks the demo data by creating the SOLs
    from the template instead of copying every single line, which
    automatically links the SO and the project together.
    
    task-5194350
    
    X-original-commit: 1c9ae3417cd23fd2c4ceddedf14f95810c29a016

---
+++ tests/test_construction_developer/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Test Construction Developer',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the Construction Developer module.""",
+    'depends': ['base'],
+    'assets': {
+        'web.assets_tests': [
+            'test_construction_developer/static/src/js/tours/industry_construction_developer_cost_nature_tour.js',
+        ],
+    },
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
