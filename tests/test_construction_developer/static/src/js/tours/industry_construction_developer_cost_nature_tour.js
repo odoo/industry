6b99e492849cfc5005e178c35888d5983c39473f
Tristan Cazier <trcaz@odoo.com>
2026-03-18 12:16:57 +0100

    [IMP] construction{_developer}: add cost nature anaysis & improve data
    This commit adds a new view to compute the margin of products/work
    items/sale order lines on projects and sale orders using an SQL view
    through a server action.
    This commit also heavily reworks the demo data by creating the SOLs
    from the template instead of copying every single line, which
    automatically links the SO and the project together.
    
    task-5194350
    
    closes odoo/industry#1796
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

---
+++ tests/test_construction_developer/static/src/js/tours/industry_construction_developer_cost_nature_tour.js
@@ -0,0 +1,99 @@
+import { registry } from '@web/core/registry';
+
+registry.category("web_tour.tours").add("industry_construction_developer_cost_nature_analysis_report_tour", {
+    url: "/odoo",
+    steps: () => [
+    {
+        "trigger": ".o_app[data-menu-xmlid='sale\\.sale_menu_root']",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_cell[name='name']:contains('S00003')",
+        "run": "click",
+    },
+    {
+        "trigger": ".oe_stat_button:contains('Cost Nature')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_group_has_content:contains('Equipment (3)')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_group_has_content:contains('Equipment / Heavy Plant / Earth Moving (3)')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_group_has_content:contains('Excavator (3)')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Ground Preparation')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Drainage System')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Foundation Footing')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Foundation Footing')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Drainage System')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('Excavator')):has([name='x_work_item_id']:contains('Ground Preparation')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_facet_remove", // button in search bar to remove default sorting options 
+        "run": "click",
+    },
+    {
+        "trigger": ".o_searchview_input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_filter_menu .o-dropdown-item:contains('Labour')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_group_by_menu .o-dropdown-item:contains('Product')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_searchview_dropdown_toggler",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_group_has_content:contains('General Labour')",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('General Labour')):has([name='x_work_item_id']:contains('Foundation Footing')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_data_row:has([name='x_product_id']:contains('General Labour')):has([name='x_work_item_id']:contains('Roofing')) .o_list_record_selector input",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_unselect_all",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_searchview_facet:contains('Labour') .o_facet_remove",
+        "run": "click",
+    },
+    {
+        "trigger": ".o_facet_remove",
+        "run": "click",
+    }
+]
+})
