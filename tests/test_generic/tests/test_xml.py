8d4b013179062e74182415645848648ebd095f7b
Francois Vasamillet (frva) <frva@odoo.com>
2026-04-09 08:32:44 +0000

    [IMP] tests: templates add warning when not updated
    Templates should be updated.
    This commit modify the generic test _check_update_status to also test templates,
    and raise a warning if those are marked as noupdate="1".
    It also removes the noupdate attributes found for the templates.
    
    TASK-6064474
    
    closes odoo/industry#1998
    
    X-original-commit: 2908d185316ee2045471ae04d32530dfba0246a9
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>
    Signed-off-by: François Vasamillet (frva) <frva@odoo.com>

--- tests/test_generic/tests/test_xml.py
+++ tests/test_generic/tests/test_xml.py
@@ -69,6 +69,8 @@ MODELS_TO_UPDATE = {
     "ir.ui.menu",
     "ir.ui.view",
     "knowledge.article",
+    "portal.entry",
+    "template",
     "theme.utils",
     "website.assets",
     "website.controller.page",
@@ -358,8 +360,8 @@ class TestEnv(IndustryCase):
             )
 
     def _check_update_status(self, root, filename):
-        for record in root.xpath("//record") + root.xpath("//function"):
-            model = record.get('model')
+        for record in root.xpath("//record") + root.xpath("//function") + root.xpath("//template"):
+            model = record.get('model') or record.tag
             noupdate = False
             parent = record.getparent()
             data_tag = False
