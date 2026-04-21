aa5ce83abc7db79d2c1c0bdb6aa6d6bb1a4672d9
dhrs-odoo <dhrs@odoo.com>
2026-04-21 12:12:27 +0530

    [FIX] .*: fix non-stored fields in related field definitions
    Before this commit we were using the non store field in the related field
    definitions but due to recent changes in the base code we are now unable to use
    those fields in the related definitions which currently raises error .
    
    So in this commit we are replacing the related non store field reference as a
    computed field .
    
    Ref PR - https://github.com/odoo/odoo/pull/259309

--- architects/__manifest__.py
+++ architects/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Architecture Firm',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Services',
     'depends': [
         'account_followup',
--- bookstore/__manifest__.py
+++ bookstore/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Bookstore',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Retail',
     'depends': [
         'account_followup',
