9b353e5d7bea2d450549c5c11d2c67a055a4e34a
vava-odoo <vava@odoo.com>
2026-04-22 11:56:12 +0000

    [FIX] *: remove position attribute in js tour steps
    X-original-commit: 1f2183a324610d23d3f8f4382caf8e530e1b3f7a

--- personal_trainer/static/src/js/my_tour.js
+++ personal_trainer/static/src/js/my_tour.js
@@ -7,7 +7,6 @@ registry.category("web_tour.tours").add("personal_trainer_knowledge_tour", {
         {
             trigger: '.o_app[data-menu-xmlid="knowledge.knowledge_menu_root"]',
             content: _t("Get on track and explore our recommendations for your Odoo usage here!"),
-            position: "bottom",
             run: "click",
         },
     ],
