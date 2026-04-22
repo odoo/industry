00e91374d2f262023824e969c5c68ec357ee69e0
dhrs-odoo <dhrs@odoo.com>
2026-04-22 07:04:58 +0000

    [FIX] test_booking_engine: fix resource_id and time_type reference
    This commit updates the test_booking_engine to comply with standard changes:
    - Replace resource_id with resource_ids to support multiple resources.
    - Rename time_type to count_as following the updated field definition in the standard.
    
    Ref Commit - https://github.com/odoo/odoo/commit/d37cf89a6ff134988b4a7c001a97973cd95a2ea8#diff-7767bd14c6d3c44c406fd04f3d1c1deda8936898a0e725b7e4050faca2436ac3
    
    X-original-commit: 6df568e7b902a13f9e78ec81505aee17d66bb153

--- tests/test_booking_engine/tests/test_overbooking.py
+++ tests/test_booking_engine/tests/test_overbooking.py
@@ -64,7 +64,7 @@ class TestOverbooking(PaymentHttpCommon):
 
         def create_planning_slot(cls):
             cls.env['planning.slot'].create({
-                'resource_id': cls.resource.id,
+                'resource_ids': [Command.link(cls.resource.id)],
                 'role_id': cls.role.id,
                 'start_datetime': cls.start_date,
                 'end_datetime': cls.end_date,
@@ -79,7 +79,7 @@ class TestOverbooking(PaymentHttpCommon):
         sale_order.action_confirm()
         slot = sale_order.order_line.planning_slot_ids
         self.assertEqual(len(slot), 1, "One planning slot should be created.")
-        self.assertEqual(slot.resource_id, self.resource, "The slot should be assigned to the available room.")
+        self.assertEqual(slot.resource_ids.id, self.resource.id, "The slot should be assigned to the available room.")
         self.assertEqual(slot.start_datetime, sale_order.rental_start_date, "Slot start should match SO rental start.")
         self.assertEqual(slot.end_datetime, sale_order.rental_return_date, "Slot end should match SO rental return.")
 
@@ -101,7 +101,7 @@ class TestOverbooking(PaymentHttpCommon):
             'date_from': self.start_date,
             'date_to': self.end_date,
             'resource_id': self.resource.id,
-            'time_type': 'leave',
+            'count_as': 'absence',
         }])
         sale_order = self.create_sale_order()
         with self.assertRaises(ValidationError, msg="This Sales Order can't be confirmed. No resources are available for the shifts in: %s." % self.product.name):
@@ -115,7 +115,7 @@ class TestOverbooking(PaymentHttpCommon):
             'name': 'Maintenance',
             'date_from': self.start_date,
             'date_to': self.end_date,
-            'time_type': 'leave',
+            'count_as': 'absence',
         }])
         sale_order = self.create_sale_order()
         with self.assertRaises(ValidationError, msg="This Sales Order can't be confirmed. No resources are available for the shifts in: %s." % self.product.name):
