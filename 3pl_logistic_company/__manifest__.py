4bd83a671e9a84703651b4ef041f50280725c453
Francois Vasamillet (frva) <frva@odoo.com>
2026-05-21 14:41:25 +0200

    [FIX] 3pl_logistic_company: fix ownerless products on all users pages
    This commit fixes the issue of products that have all requirements to be
    displayed on the website products page of the user but no assigned owners
    are displayed on the page of some owners.
    
    Issue was coming from the value of parent_id being false for some partners,
    explaining why a Falsy owner was still displayed.
    
    This also changes the use of x_owner_id from the product to using the owner_id
    of the stock quant, so that the user view the quants he has no matter the initial
    x_owner_id on the product.
    
    TASK-6234967

--- 3pl_logistic_company/__manifest__.py
+++ 3pl_logistic_company/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': '3PL Logistic Company',
     'author': 'Odoo S.A.',
-    'version': '1.2',
+    'version': '1.3',
     'category': 'Services',
     'depends': [
         'base_industry_data',
