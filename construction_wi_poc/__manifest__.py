b30e109b976b5d48ecfeda0b3af0bfd3d97104e5
Tristan Cazier <trcaz@odoo.com>
2026-06-08 15:24:56 +0200

    [ADD] construction_wi_poc: rewrite work items using MRP

---
+++ construction_wi_poc/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': 'Construction Developer',
+    'version': '2.0',
+    'category': 'Construction',
+    'depends': [
+        'base_industry_data',
+        'construction',
+        'mrp',
+        'web_studio',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/stock_location.xml',
+        'data/stock_route.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_ui_view.xml',
+        'data/base_automation.xml',
+    ],
+    'demo': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    'cloc_exclude': [
+    ],
+    'images': ['images/main.png'],
+    'license': 'OEEL-1',
+    'author': 'Odoo S.A.',
+    'url': "https://www.odoo.com/trial?industry&selected_app=construction_developer",
+    'website': "https://www.odoo.com/all-industries",
+}
