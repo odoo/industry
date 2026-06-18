aa663e85905db85162b3bf66e12fc408801098fc
Francois Vasamillet (frva) <frva@odoo.com>
2026-06-18 08:33:19 +0000

    [IMP] 3pl_logistic_company: add test for url and fix missing id
    This commit adds a test for the portal page products,
    to be sure it is not accessible when not connected but
    accessible for any user with portal access.
    
    This also fixes a missing id in a search.
    
    Task-6306283
    
    X-original-commit: b4aef86ca7961e368bff4aa6b94b31b2175e290a

---
+++ tests/test_3pl_logistic_company/tests/test_website_urls.py
@@ -0,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import Command
+from odoo.tests import HttpCase
+
+
+class UrlsWebsiteTestCase(HttpCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user_1 = cls.env['res.users'].create([{
+            'name': 'Test User 1',
+            'login': 'test_user_1',
+            'partner_id': cls.env['res.partner'].create([{
+                'name': 'Test Partner 1',
+                'email': 'test1@example.com',
+            }]).id,
+            'group_ids': [Command.link(cls.env.ref('base.group_portal').id)],
+        }])
+
+    def test_products_stock_portal_open(self):
+        with self.assertLogs(level="WARNING"):
+            response = self.url_open('/website/action/products')
+        self.assertEqual(response.status_code, 403, 'The request should be forbidden when no user is authenticated.')
+        self.authenticate('test_user_1', '')
+        response = self.url_open('/website/action/products')
+        self.assertEqual(response.status_code, 200, 'The request should be successful for authenticated user.')
