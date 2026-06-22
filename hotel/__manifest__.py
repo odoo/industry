23a2fb3483e1551c379fedb826110d8f5d95b11e
abdrahmanrashed <aras@odoo.com>
2026-06-22 16:28:54 +0200

    [IMP] booking_engine: add house keeping task assignment
    - Reworked `booking_engine` planning roles, adding new role category field to provide scalbility in handling different resource types.
    - Replaced stayover and checkout cleaning flags with points to weight how much *effort* each stay offer would require to complete.
    - Created new action and cron to automatically assign house keeping employees to house keeping tasks based on the cleaning points.
    
    task-5929205
    
    closes odoo/industry#2061
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -18,7 +18,6 @@
         'data/product_template_attribute_line.xml',
         'data/product_image.xml',
         'data/product_product.xml',
-        'data/planning_role_post.xml',
         'data/knowledge_article.xml',
         'data/knowledge_article_favorite.xml',
         'data/mail_message.xml',
@@ -26,8 +25,6 @@
         'data/res_config_settings.xml',
     ],
     'demo': [
-        'demo/res_company.xml',
-        'demo/hr_employee.xml',
         'demo/product_template_post.xml',
         'demo/pos_config.xml',
         'demo/product_attribute.xml',
