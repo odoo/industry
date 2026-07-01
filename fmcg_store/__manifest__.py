7436cd73126f84283a3199eb07c8707e6384c25a
abdrahmanrashed <aras@odoo.com>
2026-07-01 14:38:29 +0200

    [FIX] *: minor fixes
    - Replace `view_project_kanban` xpaths with `view_project_card` following: https://github.com/odoo/odoo/commit/6650a51bd613e2a4fd92828adff5d6b057148657
    - Replace `website_sale_autocomplete` with `website_address_autocomplete` following: https://github.com/odoo/odoo/commit/a2814d3f71fc1248af194a4fd30a256c9dfc5be2
    - Change booking engine field `x_rental_status` to `x_rental_order_status` to prevent label conflict with `rental_status` in `planning.slot`
    - Remove payment methods for demo payment providers following: https://github.com/odoo/odoo/pull/258854
    - Move the required attribute in `booking_engine.product.pricelist.item.x_name` field definition to views
    - Change `group_licence_management` xpath following: https://github.com/odoo/odoo/commit/5ee17858b6c5d901f04210c1de158e38eb7109e7 and https://github.com/odoo/odoo/commit/5ee17858b6c5d901f04210c1de158e38eb7109e7
    - Change `mailing_filter_id` to `mailing_filter_ids` following: https://github.com/odoo/odoo/commit/9937f092b5ea73dfa3d56e7dc034ab83a199397f
    
    closes odoo/industry#2440
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- fmcg_store/__manifest__.py
+++ fmcg_store/__manifest__.py
@@ -49,8 +49,6 @@
         'demo/purchase_order_post.xml',
         'demo/sale_order_post.xml',
         'demo/payment_provider_demo_post.xml',
-        'demo/payment_provider.xml',
-        'demo/payment_method.xml',
         'demo/pos_session.xml',
         'demo/pos_order.xml',
         'demo/pos_order_line.xml',
