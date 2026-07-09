f651f53fdad11e8e791f73be0023135abb041582
chga-odoo <chga@odoo.com>
2026-07-09 19:13:17 +0530

    [IMP] shoe_maker: remove shoe maker industry
    Task: 6357553

--- tests/test_shoe_maker/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Test Shoe Maker',
-    'category': 'Hidden/Tests',
-    'description': """A module to test code in the shoe maker industry.""",
-    'depends': ['base'],
-    'license': 'LGPL-3',
-    'author': 'Odoo S.A.',
-}
