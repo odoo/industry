36c03c24c8d0c3532f7ccb2ca10fc435d86ad2d2
Tristan Cazier <trcaz@odoo.com>
2026-07-15 12:41:53 +0200

    [REF] construction_developer: delete all

--- tests/test_construction_developer/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import test_cost_nature_analysis_report
-from . import test_cost_nature_analysis_report_tour
-from . import test_action_server
