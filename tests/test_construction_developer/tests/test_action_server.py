be575607a656ebc98975c624a64e93133069e325
chga-odoo <chga@odoo.com>
2026-07-29 11:46:20 +0000

    [FIX] construction_developer: align unit price and margin formulas
    - Redefine unit margin as a percentage of the selling price
    - Update the inverse unit price calculation to keep both formulas
      mathematically consistent
    - Update affected tests to validate the new behavior
    
    closes odoo/industry#2707
    
    Task: 6273532
    X-original-commit: fcf24275b4d3b7e1bef683c6189bc09e46dd979d
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- tests/test_construction_developer/tests/test_action_server.py
+++ tests/test_construction_developer/tests/test_action_server.py
@@ -67,21 +67,21 @@ class ActionServerTestCase(TransactionCase):
         self.assertEqual(test_template_work_item_lines[1].x_unit_price, 40.0, "Assignation error: The second work item line should have a price of 40.0")
         self.assertEqual(self.test_template_work_item.x_unit_cost, 50.0, "The cost of work item is not computed correctly: it should be the sum of the product of components costs by quantity")
         self.assertEqual(self.test_template_work_item.x_unit_price, 100.0, "The price of work item is not computed correctly: it should be the sum of the product of components prices by quantity")
-        self.assertEqual(self.test_template_work_item.x_unit_margin, 1.0, "The margin of work item is not computed correctly: it should be equal to 1.0 at this point")
+        self.assertEqual(self.test_template_work_item.x_unit_margin, 0.5, "The margin of work item is not computed correctly: it should be equal to 0.5 at this point")
         test_template_work_item_lines[0].x_unit_cost = 20
         self.assertEqual(self.test_template_work_item.x_unit_cost, 100.0, "The cost of work item is not recomputed correctly when the cost of a component changes")
         self.assertEqual(self.test_template_work_item.x_unit_price, 100.0, "The price of work item changes when the cost of a component changes but it should not happen")
         self.assertEqual(self.test_template_work_item.x_unit_margin, 0.0, "The margin of work item is not correct after the cost of a component changes: it should be recomputed to represent the new value")
-        test_template_work_item_lines[1].x_unit_price = 240
+        test_template_work_item_lines[1].x_unit_price = 340
         self.assertEqual(self.test_template_work_item.x_unit_cost, 100.0, "The cost of work item changes after the price of a component changes while it should not happen")
-        self.assertEqual(self.test_template_work_item.x_unit_price, 300.0, "The price of work item is not recomputed correctly when the price of a component changes")
-        self.assertEqual(self.test_template_work_item.x_unit_margin, 2.0, "The margin of work item is not correct after the price of a component changes: it should be recomputed to represent the new value")
+        self.assertEqual(self.test_template_work_item.x_unit_price, 400.0, "The price of work item is not recomputed correctly when the price of a component changes")
+        self.assertEqual(self.test_template_work_item.x_unit_margin, 0.75, "The margin of work item is not correct after the price of a component changes: it should be recomputed to represent the new value")
         self.test_template_work_item.x_is_margin_fixed = True
-        self.test_template_work_item.x_unit_price = 200
-        self.assertEqual(self.test_template_work_item.x_unit_margin, 1.0, "When margin is fixed, the margin should be recomputed if we change manually the price of work item")
+        self.test_template_work_item.x_unit_price = 500
+        self.assertEqual(self.test_template_work_item.x_unit_margin, 0.8, "When margin is fixed, the margin should be recomputed if we change manually the price of work item")
         self.test_template_work_item.x_unit_margin = 0.5
-        self.assertEqual(self.test_template_work_item.x_unit_price, 150.0, "When margin is fixed, the price should be recomputed if we change manually the margin of work item")
+        self.assertEqual(self.test_template_work_item.x_unit_price, 200.0, "When margin is fixed, the price should be recomputed if we change manually the margin of work item")
         test_template_work_item_lines[1].x_unit_cost = 10
         self.assertEqual(self.test_template_work_item.x_unit_margin, 0.5, "When margin is fixed, the margin of work item should stay the same when the cost of a component changes")
         self.assertEqual(self.test_template_work_item.x_unit_cost, 90.0, "When margin is fixed, the cost of work item should be recomputed when the cost of a component changes")
-        self.assertEqual(self.test_template_work_item.x_unit_price, 135.0, "When margin is fixed, the price of work item should be recomputed when the cost of a component changes")
+        self.assertEqual(self.test_template_work_item.x_unit_price, 180.0, "When margin is fixed, the price of work item should be recomputed when the cost of a component changes")
