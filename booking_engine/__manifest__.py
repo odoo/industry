fb8f5c5e940aa602af4e44aace2289b34d53480e
dhrs-odoo <dhrs@odoo.com>
2026-08-06 13:10:10 +0000

    [IMP] booking_engine: add automated survey settings
    - Removed the previous implementation for sending surveys to customers.
    - Added a new Automated Survey setting with:
       --> Survey: Select the survey to be sent to the customer.
       --> Email Delay: Define the delay before sending the survey email.
    - Once the setting is enabled, the configured survey email will be automatically
      sent to the customer after their stay is completed.
    - This allows the survey process to be configured directly from the settings.
    
    Task-6389420
    
    closes odoo/industry#2916
    
    X-original-commit: 2e6cc0e86f5bb44c77137529cc0c0eeb6ca72612
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>
    Signed-off-by: Dhrumil Shah (dhrs) <dhrs@odoo.com>

--- booking_engine/__manifest__.py
+++ booking_engine/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Booking',
-    'version': '1.17',
+    'version': '1.18',
     'category': 'Hospitality',
     'author': 'Odoo S.A.',
     'depends': [
@@ -30,6 +30,7 @@
         'data/base_automation.xml',
         'data/resource_calendar_data.xml',
         'data/ir_cron.xml',
+        'data/survey_survey.xml',
         'data/res_config_settings.xml',
         'data/ir_actions_act_window.xml',
         'data/ir_ui_view.xml',
@@ -40,7 +41,6 @@
         'data/product_attribute.xml',
         'data/product_attribute_value.xml',
         'data/product_pricelist_item.xml',
-        'data/survey_survey.xml',
         'data/survey_question.xml',
         'data/survey_question_answer.xml',
     ],
