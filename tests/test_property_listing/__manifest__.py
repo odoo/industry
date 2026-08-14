2967b876042d822864f431b9449f2ef227997628
amas-odoo <amas@odoo.com>
2026-08-14 18:22:41 +0530

    [IMP] tests: Update tests for Property Listing
    Move the related tests from real_estate to property_listing to match the
    relocation of the automations and server actions from the Real Estate industry
    to the Property Listing module.

---
+++ tests/test_property_listing/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Test Property Listing',
+    'category': 'Hidden/Tests',
+    'description': """A module to test code in the industry property listing.""",
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author': 'Odoo S.A.',
+}
