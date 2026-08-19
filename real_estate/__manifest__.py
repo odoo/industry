64f2742f76646bff4294a8cd37cb8822f9b12e37
Tom Cobut <toco@odoo.com>
2026-08-19 13:44:19 +0200

    [IMP] real_estate: improve design of demo website
    This commit adds a demo website into the /real_estate industry module. When the user installs the industry with demo data, it creates a second website with a cutting edge look and feel to demonstrate what users can achieve with the Website Builder.
    
    Task-5478148
    
    closes odoo/industry#2794
    
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Real Estate Agency',
-    'version': '2.0',
+    'version': '2.1',
     'category': 'Services',
     'author': 'Odoo S.A.',
     'depends': [
@@ -49,17 +49,34 @@
         'data/website_view.xml',
         'data/website_page.xml',
         'data/website_menu.xml',
-        'data/website_theme_apply.xml',
         'data/uninstall_hook.xml',
         'data/website.xml',
     ],
     'demo': [
+        # The website record comes first: property_listing (a dependency) already
+        # created it and repointed `base.default_website` onto it; this file
+        # only adds real_estate's own customizations to that same record.
+        'demo/website/website.xml',
+        'demo/website/views/website_templates.xml',
+        'demo/website/views/website_sale_templates.xml',
+        # Before any page: deactivates the generic website.homepage /
+        # website.contactus views by copy-on-write.
+        'demo/website/server_actions.xml',
+        'demo/website/assets.xml',
+        'demo/website/images.xml',
+        'demo/website/pages/contactus.xml',
+        'demo/website/pages/matchmaking.xml',
         'demo/res_company.xml',
+        'demo/res_users.xml',
         'demo/product_attribute_value.xml',
         'demo/res_partner.xml',
         'demo/hr_department.xml',
         'demo/hr_employee.xml',
         'demo/product_template.xml',
+        # After product_template.xml: the page's own <function> substitutes the raw
+        # /shop/<slug>-<id> links the export snapshotted, so the property templates
+        # it points at have to exist already.
+        'demo/website/pages/home.xml',
         'demo/product_document.xml',
         'demo/product_image.xml',
         'demo/product_template_attribute_line.xml',
@@ -75,16 +92,17 @@
         'demo/project_task.xml',
         'demo/sale_commission_plan_user.xml',
         'demo/ir_attachment_post.xml',
-        'demo/website_view.xml',
-        'demo/website_page.xml',
-        'demo/website_theme_apply.xml',
-        'demo/website.xml',
     ],
     "cloc_exclude": [
         "data/knowledge_article.xml",
         "data/uninstall_hook.xml",
         "data/website_view.xml",
-        "demo/website_view.xml",
+        "demo/website/pages/home.xml",
+        "demo/website/pages/contactus.xml",
+        "demo/website/pages/matchmaking.xml",
+        "demo/website/views/website_templates.xml",
+        "demo/website/views/website_sale_templates.xml",
+        "static/src/scss/primary_variables.scss",
     ],
     'application': True,
     'images': ['images/main.png'],
