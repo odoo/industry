c482c4d77736c1e49dd84367445fb9e13726088e
Tristan Cazier <trcaz@odoo.com>
2026-09-10 14:16:12 +0200

    [IMP] construction_developer: overhead costs
    This commit extends the SO discount to proportionally scale the SOLs so that the total grows for a fixed amount. It adds a new button in the discount modal and a field at the bottom of the SO to keep track of the total discount applied.
    
    task-5945921

--- construction_developer/__manifest__.py
+++ construction_developer/__manifest__.py
@@ -36,6 +36,7 @@
         'features/project/so_o2m.xml',
         'features/mrp/delivery_progress.xml',
         'features/sale/contract_type.xml',
+        'features/sale/discount_split_on_lines.xml',
         'features/mrp/work_breakdown_structure/override_deadline_with_custom_field.xml',
         'features/mrp/work_breakdown_structure/work_breakdown_structure.xml',
 
