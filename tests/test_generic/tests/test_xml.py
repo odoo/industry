755d561b5d40dfce72f6865c218805ac262e6a45
vava-odoo <vava@odoo.com>
2026-09-15 08:00:03 +0000

    [FIX] test_generic: fix demo company update test
    Previous commit[^1] introduced an error while testing a no-demo
    database. In production, the industry doesn't edit the main company and
    this test is supposed to run only in demo environment. This commit fixes
    the issue by adding a check on the demo validity before throwing a
    warning in the logs.
    
    [^1]: https://github.com/odoo/industry/commit/bbec9fecdb359c3d9087c3703a8bdaba6e1454ae
    
    closes odoo/industry#2970
    
    X-original-commit: 81f317909ccbdc2b6249d4a336b599b1354f5445
    Signed-off-by: François Vasamillet (frva) <frva@odoo.com>
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- tests/test_generic/tests/test_xml.py
+++ tests/test_generic/tests/test_xml.py
@@ -153,6 +153,7 @@ class TestEnv(IndustryCase):
         in_use_files = set()
         checked_records_with_user = {}
         manifest_content = None
+        with_demo = self.env['ir.module.module'].search_count([('demo', '=', True)], limit=1)
         for root, dirs, files in os.walk(path):
             # sort the directory by alphabetical order so static directory is read first.
             dirs.sort(reverse=True)
@@ -177,7 +178,7 @@ class TestEnv(IndustryCase):
                         manifest_content = decoded_content
                     continue
 
-                if root.split('/')[-1] == 'demo' and not self.env['ir.module.module'].search_count([('demo', '=', True)], limit=1):
+                if root.split('/')[-1] == 'demo' and not with_demo:
                     continue
                 try:
                     tree = etree.fromstring(encoded_content)
@@ -214,7 +215,7 @@ class TestEnv(IndustryCase):
                 if root.split('/')[-1] == 'demo':
                     has_main_company_record = has_main_company_record or self._check_main_company_inherit_is_present(tree, file_name, module)
 
-        if not has_main_company_record and module in self.installed_industries:
+        if not has_main_company_record and module in self.installed_industries and with_demo:
             _logger.warning(
                 "The module %s does not contain an extension of main_company."
                 "It should change the name and logo of the company to the industry ones.",
