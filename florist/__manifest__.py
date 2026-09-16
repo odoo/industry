94b4ad683e271035565c6408ee5163b5c6604430
aycha-odoo <aycha@odoo.com>
2026-09-16 14:26:35 +0530

    [IMP] florist: removed the inventory module and reorder apps
    Reorder the main app menus to provide a consistent navigation flow for
    the florist application and remove the Inventory app, which is not
    required.
    
    Task-6264296

--- florist/__manifest__.py
+++ florist/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'Florist',
-    'version': '1.2',
+    'version': '1.3',
     'category': 'Retail',
     'author': 'Odoo S.A.',
     'depends': [
         'base_industry_data',
         'crm_enterprise',
         'pos_online_payment',
-        'pos_stock',
         'project_enterprise',
         'sale_crm',
         'sale_loyalty',
@@ -16,6 +15,7 @@
         'website_crm',
     ],
     'data': [
+        'data/ir_ui_menu.xml',
         'data/res_config_settings.xml',
         'data/ir_attachment_pre.xml',
         'data/pos_category.xml',
