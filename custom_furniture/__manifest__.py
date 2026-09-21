874b6de9e42119f7f59ccbb69f68b82c13c2e9ed
vava-odoo <vava@odoo.com>
2026-09-21 19:41:21 +0000

    [REM] *: remove duplicated tour
    Since 19.3, headhunter inherits from base_industry_data. The js tour is
    therefore duplicated in both modules. This commit removes it in
    headhunter and several other similar modules
    
    closes odoo/industry#3051
    
    X-original-commit: 2d237ef55d312256b7d9a9dd18450b04046acc98
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- custom_furniture/__manifest__.py
+++ custom_furniture/__manifest__.py
@@ -47,7 +47,6 @@
         'data/knowledge_article_favorite.xml',
         'data/mail_message.xml',
         'data/quality_point.xml',
-        'data/knowledge_tour.xml',
     ],
     'demo': [
         'demo/res_company.xml',
@@ -70,15 +69,9 @@
         'demo/quality_check.xml',
         'demo/planning_slot.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'custom_furniture/static/src/js/my_tour.js',
-        ]
-    },
     'author': 'Odoo S.A.',
     "cloc_exclude": [
         "data/knowledge_article.xml",
-        "static/src/js/my_tour.js",
     ],
     'license': 'OEEL-1',
     'application': True,
